fbaa321f5ac7e6b5d59e045d14f6ee2a781932c7
sam <sam@sksamuel.com>
2013-02-05 22:58:03 +0000

    Added jquery calender plugins

--- src/main/scala/com/liferay/scalapress/service/theme/tag/general/general.scala
+++ src/main/scala/com/liferay/scalapress/service/theme/tag/general/general.scala
@@ -37,7 +37,9 @@ object ScriptTag extends ScalapressTag with TagBuilder {
           <script src="//cdn.jsdelivr.net/colorbox/1.3.21/jquery.colorbox.js"></script>
           <script src="/static/js/jquery.easing.1.3.js"></script>
           <script src="/static/js/jquery.galleryview-3.0-dev.js"></script>
-          <script src="/static/js/jquery.timers-1.2.js"></script>"""
+          <script src="/static/js/jquery.timers-1.2.js"></script>
+          <script src="/static/js/jquery.eventCalendar.js" type="text/javascript"></script>
+                  """
         Some(tag)
     }
 }
@@ -46,7 +48,9 @@ object CssTag extends ScalapressTag with TagBuilder {
     def render(request: ScalapressRequest, context: ScalapressContext, params: Map[String, String]) = {
         val tag = """<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
                      <link href="/static/css/jquery.galleryview-3.0-dev.css" rel="stylesheet"/>
-                    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'/>"""
+                    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'/>
+                    	<link rel="stylesheet" href="/static/css/eventCalendar.css">
+                    	<link rel="stylesheet" href="/static/css/eventCalendar_theme_responsive.css">"""
         Some(tag)
     }
 }
