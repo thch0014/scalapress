038d280e026316d50ca88515547444fbcf475993
sam <sam@sksamuel.com>
2013-02-15 01:41:23 +0000

    added basic listing packages

---
+++ src/main/scala/com/liferay/scalapress/plugin/listings/ListingPackage.scala
@@ -0,0 +1,29 @@
+package com.liferay.scalapress.plugin.listings
+
+import javax.persistence.{Column, ManyToOne, Entity, Table, GenerationType, GeneratedValue, Id}
+import reflect.BeanProperty
+import com.liferay.scalapress.domain.ObjectType
+
+/** @author Stephen Samuel */
+@Entity
+@Table(name = "listings_packages")
+class ListingPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @BeanProperty var id: Long = _
+
+    @Column(name = "maxcategories")
+    @BeanProperty var maxFolders: Int = _
+
+    @BeanProperty var maxImages: Int = _
+
+    @BeanProperty var maxCharacters: Int = _
+
+    @BeanProperty var labels: String = _
+
+    @BeanProperty var fee: Int = _
+
+    @ManyToOne
+    @BeanProperty var objectType: ObjectType = _
+}
