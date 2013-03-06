1ba82e290d01e100cd03401448e8f96260337bc4
sam <sam@sksamuel.com>
2013-03-06 08:42:15 +0000

    Fixed search form refresh bug. Added 30 minute cron for search indexes

--- src/main/scala/com/liferay/scalapress/feeds/gbase/GBaseCron.scala
+++ src/main/scala/com/liferay/scalapress/feeds/gbase/GBaseCron.scala
@@ -18,7 +18,7 @@ class GBaseCron extends CronTask with Logging {
     @Autowired var objectDao: ObjectDao = _
     @Autowired var installationDao: InstallationDao = _
 
-    @Scheduled(cron = "0 0 2,12,18 * * *")
+    @Scheduled(cron = "0 0 12,18 * * *")
     @Transactional
     def run() {
         logger.info("Running GBase Cron")
