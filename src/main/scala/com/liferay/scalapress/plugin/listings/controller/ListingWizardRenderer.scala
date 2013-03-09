04ecc585cb1a2eeeb58785aed07b37080e95a39d
sam <sam@sksamuel.com>
2013-03-09 14:18:35 +0000

    Added package step to wizards

--- src/main/scala/com/liferay/scalapress/plugin/listings/controller/ListingWizardRenderer.scala
+++ src/main/scala/com/liferay/scalapress/plugin/listings/controller/ListingWizardRenderer.scala
@@ -5,16 +5,15 @@ import com.liferay.scalapress.service.{WizardRenderer, WizardStep}
 /** @author Stephen Samuel */
 object ListingWizardRenderer {
 
-    //  val ChoosePackage = 1
-    val SelectFolder = 1
-    val ListingFields = 2
-    val UploadImages = 3
-    val Confirmation = 4
-    val Completed = 5
+    val ChoosePackage = 1
+    val SelectFolder = 2
+    val ListingFields = 3
+    val UploadImages = 4
+    val Confirmation = 5
+    val Completed = 6
 
-    // WizardStep("/listing/package", "Package"),
-
-    def steps = List(WizardStep("/listing/folder", "Folders"),
+    def steps = List(WizardStep("/listing/package", "Package"),
+        WizardStep("/listing/folder", "Folders"),
         WizardStep("/listing/field", "Details"),
         WizardStep("/listing/image", "Images"),
         WizardStep("/listing/confirmation", "Confirm"),
