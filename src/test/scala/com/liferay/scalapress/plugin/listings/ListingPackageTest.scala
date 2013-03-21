8c04d71ddf5d52b9bcf1c17a9e8ec24b66bc4d30
sksamuel <sam@sksamuel.com>
2013-03-21 22:27:34 +0000

    Fixed tests

--- src/test/scala/com/liferay/scalapress/plugin/listings/ListingPackageTest.scala
+++ src/test/scala/com/liferay/scalapress/plugin/listings/ListingPackageTest.scala
@@ -14,9 +14,9 @@ class ListingPackageTest extends FunSuite {
 
     test("that a non-fee package is rendered properly") {
         val lp = new ListingPackage
-        lp.fee = 45
+        lp.fee = 4500
         lp.duration = 30
-        assert("£45 for 1 month" === lp.priceText)
+        assert("£45.00 for 1 month" === lp.priceText)
     }
 
     test("that a monthly package is rendered in months") {
