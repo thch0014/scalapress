1672c4dccffbbb96f62d4169373f3b76bafc7f7e
sksamuel <sam@sksamuel.com>
2013-04-28 22:02:25 +0100

    added HTML compression to scalapage rendering

--- src/main/scala/com/liferay/scalapress/util/mvc/ScalapressPageRenderer.scala
+++ src/main/scala/com/liferay/scalapress/util/mvc/ScalapressPageRenderer.scala
@@ -2,6 +2,7 @@ package com.liferay.scalapress.util.mvc
 
 import com.liferay.scalapress.ScalapressContext
 import com.liferay.scalapress.theme.tag.TagRenderer
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor
 
 /** @author Stephen Samuel */
 class ScalapressPageRenderer(context: ScalapressContext) {
@@ -9,11 +10,13 @@ class ScalapressPageRenderer(context: ScalapressContext) {
     def render(page: ScalapressPage): String = {
 
         val sb = new StringBuilder()
-
         sb.append(page._toolbar.getOrElse(""))
         sb.append(TagRenderer.render(page.theme.header, page.req, context))
         page._body.filter(_ != null).map(_.toString).filter(_ != null).foreach(sb.append(_))
         sb.append(TagRenderer.render(page.theme.footer, page.req, context))
-        sb.toString()
+        val preCompressed = sb.toString()
+
+        val compressor = new HtmlCompressor()
+        compressor.compress(preCompressed)
     }
 }
---
+++ src/test/scala/com/liferay/scalapress/util/geo/mvc/ScalapressPageRendererTest.scala
@@ -0,0 +1,30 @@
+package com.liferay.scalapress.util.geo.mvc
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import com.liferay.scalapress.util.mvc.{ScalapressPageRenderer, ScalapressPage}
+import com.liferay.scalapress.{ScalapressRequest, ScalapressContext}
+import javax.servlet.http.HttpServletRequest
+import com.liferay.scalapress.theme.Theme
+
+/** @author Stephen Samuel */
+class ScalapressPageRendererTest extends FunSuite with MockitoSugar with OneInstancePerTest {
+
+    val context = new ScalapressContext
+    val req = mock[HttpServletRequest]
+    val sreq = ScalapressRequest(req, context)
+
+    test("output is compresssed") {
+        val page = new ScalapressPage(new Theme, sreq)
+        page.body("some body                with lots of \n\n\n\n\n\n whitespace")
+        val output = new ScalapressPageRenderer(context).render(page)
+        assert("some body with lots of whitespace" === output)
+    }
+
+    test("tags are kept") {
+        val page = new ScalapressPage(new Theme, sreq)
+        page.body("some body     <a href=\"/somelink\">click me</a>           with lots of \n\n\n\n\n\n whitespace")
+        val output = new ScalapressPageRenderer(context).render(page)
+        assert("some body <a href=\"/somelink\">click me</a> with lots of whitespace" === output)
+    }
+}
