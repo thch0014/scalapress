e754247035c910d13c17e557e645367be8bcf0f8
sksamuel <sam@sksamuel.com>
2013-05-07 08:47:44 +0100

    Added tests for smush plugin

---
+++ src/test/scala/com/liferay/scalapress/plugin/smush/SmushPluginTest.scala
@@ -0,0 +1,23 @@
+package com.liferay.scalapress.plugin.smush
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import java.io.ByteArrayInputStream
+
+/** @author Stephen Samuel */
+class SmushPluginTest extends FunSuite with MockitoSugar with OneInstancePerTest {
+
+    test("js is minified") {
+
+        val js = "var a = Array();            var b         =Array(); "
+        val actual = new SmushPlugin()._minifyJs(new ByteArrayInputStream(js.getBytes("UTF-8")))
+        assert("var a=Array();var b=Array();" === new String(actual, "utf-8"))
+    }
+
+    test("css is minified") {
+
+        val js = ".class { background-color: #FFFFFF;          text-weight: BOLD;  } "
+        val actual = new SmushPlugin()._minifyCss(new ByteArrayInputStream(js.getBytes("UTF-8")))
+        assert(".class{background-color:#fff;text-weight:BOLD}" === new String(actual, "utf-8"))
+    }
+}
