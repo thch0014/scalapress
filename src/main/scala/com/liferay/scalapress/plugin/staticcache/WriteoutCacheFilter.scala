3f79ae55088ea53084b600a218da061e8dc41ca3
sksamuel <sam@sksamuel.com>
2013-05-08 21:16:39 +0100

    Updated writeout cache to ignore files with query param

--- src/main/scala/com/liferay/scalapress/plugin/staticcache/WriteoutCacheFilter.scala
+++ src/main/scala/com/liferay/scalapress/plugin/staticcache/WriteoutCacheFilter.scala
@@ -38,34 +38,41 @@ class WriteoutCacheFilter extends Filter with Logging {
         val uri = request.asInstanceOf[HttpServletRequest].getRequestURI
 
         // we only want to cache non-resource files, easist way is to look for files without an extension
-        // as they will be proper spring controllers
+        // as they will be proper spring controllers, and only ones without query param
         Option(FilenameUtils.getExtension(uri)) match {
 
             case Some(extension) if extension.length > 0 => chain.doFilter(request, response)
             case _ =>
 
-                val filename = uri.replace("-", "_").replace("/", "_")
-                val path = context.getRealPath(properties.directory + "/" + filename)
-                logger.debug("Cache check [{}]", path)
-                val file = new File(path)
-                if (file.exists && file.lastModified > System.currentTimeMillis - properties.timeout * 1000l) {
+                Option(request.asInstanceOf[HttpServletRequest].getQueryString) match {
 
-                    logger.debug("Cache hit [{}]", path)
-                    val input = FileUtils.openInputStream(file)
-                    IOUtils.copy(input, response.getOutputStream)
+                    case Some(query) => chain.doFilter(request, response)
+                    case _ =>
 
-                } else {
 
-                    val branch = new ByteArrayOutputStream
-                    val tee = new TeeServletOutputStream(new TeeOutputStream(response.getOutputStream, branch))
-                    val wrapper = new HttpServletResponseWrapper(response.asInstanceOf[HttpServletResponse]) {
-                        override def getOutputStream: ServletOutputStream = tee
-                    }
-                    chain.doFilter(request, wrapper)
+                        val filename = uri.replace("-", "_").replace("/", "_")
+                        val path = context.getRealPath(properties.directory + "/" + filename)
+                        logger.debug("Cache check [{}]", path)
+                        val file = new File(path)
+                        if (file.exists && file.lastModified > System.currentTimeMillis - properties.timeout * 1000l) {
 
-                    val output = IOUtils.toString(branch.toByteArray, "utf-8")
-                    logger.debug("Cache fail - Writing {} chars", output.length)
-                    FileUtils.write(file, output)
+                            logger.debug("Cache hit [{}]", path)
+                            val input = FileUtils.openInputStream(file)
+                            IOUtils.copy(input, response.getOutputStream)
+
+                        } else {
+
+                            val branch = new ByteArrayOutputStream
+                            val tee = new TeeServletOutputStream(new TeeOutputStream(response.getOutputStream, branch))
+                            val wrapper = new HttpServletResponseWrapper(response.asInstanceOf[HttpServletResponse]) {
+                                override def getOutputStream: ServletOutputStream = tee
+                            }
+                            chain.doFilter(request, wrapper)
+
+                            val output = IOUtils.toString(branch.toByteArray, "utf-8")
+                            logger.debug("Cache fail - Writing {} chars", output.length)
+                            FileUtils.write(file, output)
+                        }
                 }
         }
     }
