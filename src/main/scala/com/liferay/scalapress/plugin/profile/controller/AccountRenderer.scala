237f0cf1859a831493b2e9fd0a4450b7ce85e821
sksamuel <sam@sksamuel.com>
2013-05-15 08:07:32 +0100

    Added account logout link

--- src/main/scala/com/liferay/scalapress/plugin/profile/controller/AccountRenderer.scala
+++ src/main/scala/com/liferay/scalapress/plugin/profile/controller/AccountRenderer.scala
@@ -6,7 +6,10 @@ import com.liferay.scalapress.ScalapressContext
 /** @author Stephen Samuel */
 object AccountRenderer {
     def links(links: Seq[Class[AccountLink]], context: ScalapressContext): String = {
-        links.map(klass => klass.newInstance)
+
+        val more = links :+ classOf[LogoutAccountLink]
+
+        more.map(klass => klass.newInstance)
           .filter(_.accountLinkEnabled(context))
           .map(link => {
             <a href={link.profilePageLinkUrl} class="btn btn-info" id={link.profilePageLinkId}>
@@ -14,4 +17,11 @@ object AccountRenderer {
             </a>
         }).mkString("<br/>")
     }
+}
+
+class LogoutAccountLink extends AccountLink {
+    def accountLinkEnabled(context: ScalapressContext): Boolean = true
+    def profilePageLinkId: String = "accountlink-logout"
+    def profilePageLinkUrl: String = "/j_spring_security_logout"
+    def profilePageLinkText: String = "Logout"
 }
