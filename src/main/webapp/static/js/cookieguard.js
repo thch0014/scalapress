b4953231315134fbad9511e08b77192b43dc1a61
sksamuel <sam@sksamuel.com>
2013-05-17 21:20:09 +0100

    removed some old js

--- src/main/webapp/static/js/cookieguard.js
+++
@@ -1,8 +0,0 @@
-<script>
-  $(document).ready(function(){
-    $.cookieguard();
-    $.cookieguard.cookies.add('Google Analytics', '__utma,__utmb,__utmc,__utmz,__utmv', 'These cookies are used to collect information about how visitors use our site. We use the information to compile reports and to help us improve the site. The cookies collect information in an anonymous form, including the number of visitors to the site, where visitors have come to the site from and the pages they visited.', false);
-    $.cookieguard.cookies.add('Java/JSP Session', 'JSESSIONID', 'This cookie is used to track important logical information for the smooth operation of the site', true);
-    $.cookieguard.run();
-  });
-</script>
