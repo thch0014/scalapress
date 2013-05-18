fc788be8fc124473f376723ba40a5d820eafbb31
sksamuel <sam@sksamuel.com>
2013-05-18 22:36:14 +0100

    Implemented tags widget

--- src/main/scala/com/liferay/scalapress/plugin/search/tags/TagsWidget.scala
+++ src/main/scala/com/liferay/scalapress/plugin/search/tags/TagsWidget.scala
@@ -3,16 +3,33 @@ package com.liferay.scalapress.plugin.search.tags
 import com.liferay.scalapress.widgets.Widget
 import com.liferay.scalapress.ScalapressRequest
 import javax.persistence.{Entity, Table}
-import com.liferay.scalapress.search.SavedSearch
+import com.liferay.scalapress.search.{SearchService, SavedSearch}
+import scala.xml.Utility
 
 /** @author Stephen Samuel */
 @Table(name = "plugin_tagswidget")
 @Entity
 class TagsWidget extends Widget {
+
     def render(req: ScalapressRequest): Option[String] = {
+
         val search = new SavedSearch
-        search.facets = Seq("tags")
+        search.facets = Seq(SearchService.FACET_TAGS)
+
         val result = req.context.searchService.search(search)
-        None
+        val tags = result.facets.find(_.name == SearchService.FACET_TAGS) match {
+            case None => Nil
+            case Some(facet) => facet.terms.map(_.term)
+        }
+
+        val xml = <ul class="tags-widget">
+            {_renderTags(tags)}
+        </ul>
+        Some(xml.toString())
     }
+
+    def _renderTags(tags: Iterable[String]) =
+        tags.map(tag => <span>
+            {tag}
+        </span>).map(Utility.trim(_))
 }
--- src/main/scala/com/liferay/scalapress/search/SearchService.scala
+++ src/main/scala/com/liferay/scalapress/search/SearchService.scala
@@ -14,8 +14,12 @@ trait SearchService {
     @deprecated def index()
 }
 
+object SearchService {
+    val FACET_TAGS = "tags"
+}
+
 case class SearchResult(refs: Seq[ObjectRef] = Nil, facets: Seq[Facet] = Nil, count: Int = 0)
-case class Facet(name: String, values: Seq[FacetTerm])
+case class Facet(name: String, terms: Seq[FacetTerm])
 case class FacetTerm(term: String, count: Int)
 case class ObjectRef(id: Long,
                      objectType: Long,
