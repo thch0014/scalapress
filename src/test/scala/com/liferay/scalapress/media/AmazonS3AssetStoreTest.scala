b8e2942961897c9e1c0701e86c60b2af12cbb892
sksamuel <sam@sksamuel.com>
2013-05-18 12:22:03 +0100

    Fixed compile error in SPconfig

--- src/test/scala/com/liferay/scalapress/media/AmazonS3AssetStoreTest.scala
+++ src/test/scala/com/liferay/scalapress/media/AmazonS3AssetStoreTest.scala
@@ -2,7 +2,7 @@ package com.liferay.scalapress.media
 
 import org.scalatest.FunSuite
 import org.scalatest.mock.MockitoSugar
-import com.liferay.scalapress.plugin.amazons3.AmazonS3AssetStore
+import com.liferay.scalapress.plugin.asset.amazons3.AmazonS3AssetStore
 
 /** @author Stephen Samuel */
 class AmazonS3AssetStoreTest extends FunSuite with MockitoSugar {
