155bde433591054084bfa75e2eb0fe49af2be00c
sksamuel <sam@sksamuel.com>
2013-05-19 22:25:29 +0100

    Added stock and object link tests

---
+++ src/test/scala/com/liferay/scalapress/obj/tag/LinkTagTest.scala
@@ -0,0 +1,35 @@
+package com.liferay.scalapress.obj.tag
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import com.liferay.scalapress.{ScalapressRequest, ScalapressContext}
+import javax.servlet.http.HttpServletRequest
+import com.liferay.scalapress.obj.Obj
+import com.liferay.scalapress.folder.Folder
+
+/** @author Stephen Samuel */
+class LinkTagTest extends FunSuite with OneInstancePerTest with MockitoSugar {
+
+    val context = new ScalapressContext()
+    val req = mock[HttpServletRequest]
+
+    val obj = new Obj
+    obj.id = 12
+    obj.name = "meatballs"
+
+    val f = new Folder
+    f.id = 435
+    f.name = "italian foods"
+
+    test("obj is used if set") {
+        val sreq = ScalapressRequest(req, context).withObject(obj)
+        val actual = LinkTag.render(sreq, context, Map.empty).get
+        assert("/object-12-meatballs" === actual)
+    }
+
+    test("folder is used if set") {
+        val sreq = ScalapressRequest(req, context).withFolder(f)
+        val actual = LinkTag.render(sreq, context, Map.empty).get
+        assert("/folder-435-italian-foods" === actual)
+    }
+}
---
+++ src/test/scala/com/liferay/scalapress/obj/tag/StockTagTest.scala
@@ -0,0 +1,39 @@
+package com.liferay.scalapress.obj.tag
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import com.liferay.scalapress.{ScalapressRequest, ScalapressContext}
+import javax.servlet.http.HttpServletRequest
+import com.liferay.scalapress.obj.Obj
+import com.liferay.scalapress.plugin.ecommerce.{ShoppingPlugin, ShoppingPluginDao}
+import org.mockito.Mockito
+import com.liferay.scalapress.enums.StockMethod
+
+/** @author Stephen Samuel */
+class StockTagTest extends FunSuite with OneInstancePerTest with MockitoSugar {
+
+    val context = new ScalapressContext()
+    val req = mock[HttpServletRequest]
+    context.shoppingPluginDao = mock[ShoppingPluginDao]
+    val plugin = new ShoppingPlugin
+    Mockito.when(context.shoppingPluginDao.get).thenReturn(plugin)
+
+    val obj = new Obj
+    obj.id = 12
+    obj.name = "meatballs"
+    obj.stock = 25
+
+    test("manual stock shows number as is") {
+        plugin.stockMethod = StockMethod.Manual
+        val sreq = ScalapressRequest(req, context).withObject(obj)
+        val actual = new ObjectStockTag().render(sreq, context, Map.empty).get
+        assert("25" === actual)
+    }
+
+    test("InOut stock overrides > 1 to 1") {
+        plugin.stockMethod = StockMethod.InOut
+        val sreq = ScalapressRequest(req, context).withObject(obj)
+        val actual = new ObjectStockTag().render(sreq, context, Map.empty).get
+        assert("1" === actual)
+    }
+}
