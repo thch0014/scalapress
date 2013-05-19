48540ca320a9d03158197224d94a70e826c817d2
sksamuel <sam@sksamuel.com>
2013-05-19 15:14:13 +0100

    Updated listings page look n feel

--- src/main/scala/com/liferay/scalapress/plugin/listings/controller/process/renderer/ListingsRenderer.scala
+++ src/main/scala/com/liferay/scalapress/plugin/listings/controller/process/renderer/ListingsRenderer.scala
@@ -2,15 +2,21 @@ package com.liferay.scalapress.plugin.listings.controller.process.renderer
 
 import com.liferay.scalapress.obj.Obj
 import com.liferay.scalapress.plugin.friendlyurl.FriendlyUrlGenerator
+import org.joda.time.DateTime
 
 /** @author Stephen Samuel */
 object ListingsRenderer {
 
-    def createListing = <a href="/listing/package" class="btn btn-info">Add new listing</a>
+    def createListing = <a href="/listing/package" class="btn">
+        <i class="icon-plus"></i>
+        Add new listing</a>
 
     def myListings(objects: Iterable[Obj]) = {
 
-        val elems = objects.map(obj =>
+        val elems = objects.map(obj => {
+
+            val expiryString = if (obj.expiry == 0) "" else new DateTime(obj.expiry).toString("dd/MMM/yyyy")
+
             <tr>
                 <td>
                     <a href={FriendlyUrlGenerator.friendlyUrl(obj)}>
@@ -21,15 +27,21 @@ object ListingsRenderer {
                     {obj.status}
                 </td>
                 <td>
-                    {obj.status}
+                    {expiryString}
                 </td>
                 <td>
-                    <a href={"/listing/" + obj.id}>Edit</a>
+                    <a href={"/listing/" + obj.id} class="btn btn-mini">Edit</a>
                 </td>
-            </tr>)
+            </tr>
+        })
 
         <table id="mylistings" class="table table-hover table-condensed">
-            {elems}
+            <tr>
+                <th>Listing</th>
+                <th>Status</th>
+                <th>Expiry Date</th>
+                <th>Edit</th>
+            </tr>{elems}
         </table>
     }
 }
