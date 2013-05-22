edbaeadaf9ff7fbb247f9bc8a5fc3a815df50487
sksamuel <sam@sksamuel.com>
2013-05-22 08:21:31 +0100

    Finished caroufresel section

--- src/main/scala/com/liferay/scalapress/plugin/carousel/caroufredsel/CaroufredselSection.scala
+++ src/main/scala/com/liferay/scalapress/plugin/carousel/caroufredsel/CaroufredselSection.scala
@@ -3,21 +3,24 @@ package com.liferay.scalapress.plugin.carousel.caroufredsel
 import com.liferay.scalapress.section.Section
 import javax.persistence._
 import com.liferay.scalapress.ScalapressRequest
-import scala.Array
 import scala.beans.BeanProperty
+import java.util
+import scala.collection.JavaConverters._
 
 /** @author Stephen Samuel */
 @Entity
-@Table(name = "plugin_caroufredsel")
+@Table(name = "plugin_carousel_caroufredsel")
 class CaroufredselSection extends Section {
 
     override def backoffice: String = "/backoffice/plugin/caroufredsel/section/" + id
+    def desc: String = "Caroufredsel is a circular responsive jQuery carousel."
 
-    @BeanProperty var images: Array[String] = Array()
+    @ElementCollection
+    @BeanProperty var images: java.util.Set[String] = new util.HashSet[String]()
 
     def render(request: ScalapressRequest): Option[String] = {
 
-        val renderedImages = images.map(image => {
+        val renderedImages = _images.map(image => {
             val src = request.context.assetStore.link(image)
             <li>
                 <img src={src}/>
@@ -43,5 +46,8 @@ class CaroufredselSection extends Section {
         Some(html.toString() + "\n" + script)
     }
 
-    def desc: String = "Caroufredsel is a circular responsive jQuery carousel."
+    def _images: Iterable[String] = images.size match {
+        case 0 => Option(obj).map(_.images.asScala.map(_.filename)).getOrElse(Nil)
+        case _ => images.asScala
+    }
 }
--- src/main/scala/com/liferay/scalapress/plugin/carousel/caroufredsel/CaroufredselSectionController.scala
+++ src/main/scala/com/liferay/scalapress/plugin/carousel/caroufredsel/CaroufredselSectionController.scala
@@ -1,29 +1,41 @@
 package com.liferay.scalapress.plugin.carousel.caroufredsel
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{PathVariable, ModelAttribute, RequestMethod, RequestMapping}
+import org.springframework.web.bind.annotation._
 import org.springframework.beans.factory.annotation.Autowired
 import com.liferay.scalapress.ScalapressContext
-import scala.Array
-import com.liferay.scalapress.section.SectionDao
+import org.springframework.web.multipart.MultipartFile
 
 /** @author Stephen Samuel */
 @Controller
 @RequestMapping(Array("backoffice/plugin/caroufredsel/section/{id}"))
 class CaroufredselSectionController {
 
-    @Autowired var sectionDao: SectionDao = _
     @Autowired var context: ScalapressContext = _
 
     @RequestMapping(method = Array(RequestMethod.GET))
-    def edit(@ModelAttribute("section") section: CaroufredselSection) = "admin/plugin/caroufredsel/section/edit.vm"
+    def edit(@ModelAttribute("section") section: CaroufredselSection) = "admin/plugin/carousel/caroufredsel/section/edit.vm"
 
     @RequestMapping(method = Array(RequestMethod.POST))
-    def save(@ModelAttribute("section") section: CaroufredselSection) = {
-        sectionDao.save(section)
+    def save(@ModelAttribute("section") section: CaroufredselSection,
+             @RequestParam(value = "upload", required = false) upload: MultipartFile) = {
+
+        if (upload != null && !upload.isEmpty) {
+            val key = context.assetStore.add(upload.getOriginalFilename, upload.getInputStream)
+            section.images.add(key)
+        }
+
+        context.sectionDao.save(section)
+        edit(section)
+    }
+
+    @RequestMapping(value = Array("delete/{filename}"))
+    def deleteImage(@ModelAttribute("section") section: CaroufredselSection, @PathVariable("filename") filename: String) = {
+        section.images.remove(filename)
+        context.sectionDao.save(section)
         edit(section)
     }
 
     @ModelAttribute("section") def section(@PathVariable("id") id: Long): CaroufredselSection =
-        sectionDao.find(id).asInstanceOf[CaroufredselSection]
+        context.sectionDao.find(id).asInstanceOf[CaroufredselSection]
 }
