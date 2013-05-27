affa49a1c20ad7157b4faf70b09a3f8fde772f60
sksamuel <sam@sksamuel.com>
2013-05-27 19:29:28 +0100

    Added invoice tag tests

---
+++ src/test/scala/com/liferay/scalapress/plugin/ecommerce/tag/InvoiceDeliveryDetailsTagTest.scala
@@ -0,0 +1,28 @@
+package com.liferay.scalapress.plugin.ecommerce.tag
+
+import org.scalatest.{OneInstancePerTest, FunSuite}
+import org.scalatest.mock.MockitoSugar
+import com.liferay.scalapress.plugin.ecommerce.domain.Order
+import javax.servlet.http.HttpServletRequest
+import com.liferay.scalapress.{ScalapressRequest, ScalapressContext}
+import com.liferay.scalapress.plugin.ecommerce.tags.InvoiceDeliveryDetailsTag
+
+/** @author Stephen Samuel */
+class InvoiceDeliveryDetailsTagTest extends FunSuite with MockitoSugar with OneInstancePerTest {
+
+    val order = new Order
+    order.id = 51
+    order.vatable = true
+    order.deliveryDetails = "superfast delivery"
+
+    val tag = new InvoiceDeliveryDetailsTag()
+
+    val req = mock[HttpServletRequest]
+    val context = mock[ScalapressContext]
+    val sreq = new ScalapressRequest(req, context).withOrder(order)
+
+    test("tag renders delivery details") {
+        val actual = tag.render(sreq, Map.empty)
+        assert("superfast delivery" === actual.get)
+    }
+}
