6ff98b3c779c4e5b33bd7da3ca8a7093ff9c7864
sksamuel <sam@sksamuel.com>
2013-06-01 00:41:54 +0100

    Updated dashboard to show recent using DB query and not Search Service

--- src/main/scala/com/cloudray/scalapress/util/mvc/DashboardController.scala
+++ src/main/scala/com/cloudray/scalapress/util/mvc/DashboardController.scala
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping}
 import org.springframework.stereotype.Controller
 import org.springframework.beans.factory.annotation.Autowired
 import com.cloudray.scalapress.ScalapressContext
-import com.cloudray.scalapress.search.{ObjectRef, SavedSearch}
-import com.cloudray.scalapress.enums.Sort
 import scala.collection.JavaConverters._
 import com.cloudray.scalapress.security.SpringSecurityResolver
 import javax.servlet.http.HttpServletRequest
 import java.util.Properties
 import org.apache.commons.io.IOUtils
-import com.cloudray.scalapress.obj.ObjectQuery
+import com.cloudray.scalapress.obj.Obj
 
 /** @author Stephen Samuel */
 @Controller
@@ -33,15 +31,7 @@ class DashboardController {
     @ModelAttribute("user") def indexed(req: HttpServletRequest) = SpringSecurityResolver.getAdminDetails(req).user
     @ModelAttribute("folderCount") def folderCount = context.folderDao.findAll().size
 
-    @ModelAttribute("recentObjects") def recentObjects: java.util.List[ObjectRef] = {
-        context.objectDao.recent(8)
-
-        val search = new SavedSearch
-        search.sortType = Sort.Newest
-        search.maxResults = 8
-        val result = context.searchService.search(search)
-        result.refs.asJava
-    }
+    @ModelAttribute("recentObjects") def recentObjects: java.util.List[Obj] = context.objectDao.recent(8).asJava
 
     @ModelAttribute("buildNumber") def buildNumber = props.get("buildNumber")
 }
