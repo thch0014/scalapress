b773813127f1f45a17ab6988271f6676d8f30b90
sksamuel <sam@sksamuel.com>
2013-06-01 14:32:41 +0100

    Added corpus test

--- src/test/scala/com/cloudray/scalapress/search/CorpusSearchControllerTest.scala
+++ src/test/scala/com/cloudray/scalapress/search/CorpusSearchControllerTest.scala
@@ -4,11 +4,24 @@ import org.scalatest.{OneInstancePerTest, FunSuite}
 import org.scalatest.mock.MockitoSugar
 import com.cloudray.scalapress.search.controller.CorpusSearchController
 import com.cloudray.scalapress.ScalapressContext
+import org.mockito.Mockito
+import javax.servlet.http.HttpServletRequest
+import com.cloudray.scalapress.theme.ThemeService
 
 /** @author Stephen Samuel */
 class CorpusSearchControllerTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
     val controller = new CorpusSearchController
     controller.context = new ScalapressContext
+    controller.themeService = mock[ThemeService]
+    controller.service = mock[CorpusSearchService]
+
+    val req = mock[HttpServletRequest]
+
+    test("search controller invokes corpus search") {
+        Mockito.when(controller.service.search("coldplay")).thenReturn(Nil)
+        controller.search(req, "coldplay")
+        Mockito.verify(controller.service).search("coldplay")
+    }
 
 }
