6e9cd2a13330c9f3340c2c41280b6204ce37b093
sksamuel <sam@sksamuel.com>
2013-06-02 13:16:19 +0100

    Added wicked "create folder" modal

--- src/main/scala/com/cloudray/scalapress/folder/controller/admin/FolderSearchController.scala
+++ src/main/scala/com/cloudray/scalapress/folder/controller/admin/FolderSearchController.scala
@@ -1,11 +1,11 @@
 package com.cloudray.scalapress.folder.controller.admin
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping}
+import org.springframework.web.bind.annotation.{RequestParam, ModelAttribute, RequestMapping}
 import org.springframework.beans.factory.annotation.Autowired
-import scala.Array
 import com.cloudray.scalapress.ScalapressContext
 import com.cloudray.scalapress.folder.{FolderDao, Folder}
+import scala.collection.JavaConverters._
 
 /** @author Stephen Samuel */
 @Controller
@@ -28,7 +28,16 @@ class FolderSearchController {
         "redirect:/backoffice/folder"
     }
 
-    import scala.collection.JavaConverters._
+    @RequestMapping(value = Array("create"), params = Array("name"))
+    def create(@RequestParam("name") name: String): String = {
+
+        val root = folderDao.root
+        val folder = Folder(root)
+        folder.name = name
+        folderDao.save(folder)
+
+        "redirect:/backoffice/folder/" + folder.id
+    }
 
     @ModelAttribute("folders") def folders = {
         folderDao.findAll().sortBy(_.fullName).asJava
