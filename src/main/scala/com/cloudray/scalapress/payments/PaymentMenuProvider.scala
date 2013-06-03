71ba69c2973d7205ba68d6eb388ace18f09f634f
sksamuel <sam@sksamuel.com>
2013-06-03 09:19:00 +0100

    Removed obsolte siblings section

--- src/main/scala/com/cloudray/scalapress/payments/PaymentMenuProvider.scala
+++ src/main/scala/com/cloudray/scalapress/payments/PaymentMenuProvider.scala
@@ -9,11 +9,12 @@ import com.cloudray.scalapress.ScalapressContext
 /** @author Stephen Samuel */
 class PaymentMenuProvider extends MenuItemProvider {
 
-    def item(context: ScalapressContext): Option[MenuItem] =
+    def item(context: ScalapressContext): Option[MenuItem] = {
         Some(Menu("Payments", Some("icon-credit-card"), Seq(
             MenuLink("Paypal Standard", None, "/backoffice/plugin/payment/paypal/standard"),
             MenuLink("Worldpay Select Junior", None, "/backoffice/plugin/payment/worldpay/selectjunior"),
             MenuLink("Sage Pay", None, "/backoffice/plugin/sagepayform"),
             MenuDivider,
             MenuLink("Transactions", Some("icon-credit-card"), "/backoffice/tx"))))
+    }
 }
