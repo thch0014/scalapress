dddce01092773dccd4c05fe45ba3254f91c06da6
sksamuel <sam@sksamuel.com>
2013-06-13 15:44:35 +0100

    Made sections visible by default

--- src/test/scala/com/cloudray/scalapress/section/SectionTest.scala
+++ src/test/scala/com/cloudray/scalapress/section/SectionTest.scala
@@ -2,10 +2,13 @@ package com.cloudray.scalapress.section
 
 import org.scalatest.{OneInstancePerTest, FunSuite}
 import org.scalatest.mock.MockitoSugar
-import com.cloudray.scalapress.ScalapressRequest
+import com.cloudray.scalapress.{ScalapressContext, ScalapressRequest}
 
 /** @author Stephen Samuel */
 class SectionTest extends FunSuite with MockitoSugar with OneInstancePerTest {
+
+    val context = new ScalapressContext
+    context.sectionDao = mock[SectionDao]
 
     test("default backoffice url is absolute") {
         assert(new Section {
@@ -15,9 +18,11 @@ class SectionTest extends FunSuite with MockitoSugar with OneInstancePerTest {
     }
 
     test("new section is visible by default") {
-        assert(new Section {
+        val section = new Section {
             def render(req: ScalapressRequest): Option[String] = None
             def desc: String = "new section"
-        }.visible === true)
+        }
+        section.init(context)
+        assert(section.visible === true)
     }
 }
