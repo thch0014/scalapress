5195b70371fb5b8322bcf3e963b8c9e2335a1b20
sksamuel <sam@sksamuel.com>
2013-06-18 00:44:52 +0100

    Updated menus to not show deleted types

--- src/main/scala/com/cloudray/scalapress/util/mvc/interceptor/TypesInterceptor.scala
+++ src/main/scala/com/cloudray/scalapress/util/mvc/interceptor/TypesInterceptor.scala
@@ -3,8 +3,8 @@ package com.cloudray.scalapress.util.mvc.interceptor
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import org.springframework.web.servlet.ModelAndView
-import scala.collection.JavaConverters._
 import com.cloudray.scalapress.obj.TypeDao
+import scala.collection.JavaConverters._
 
 /** @author Stephen Samuel */
 class TypesInterceptor(typeDao: TypeDao) extends HandlerInterceptorAdapter {
@@ -13,6 +13,6 @@ class TypesInterceptor(typeDao: TypeDao) extends HandlerInterceptorAdapter {
                             handler: Any,
                             modelAndView: ModelAndView) {
         if (modelAndView != null)
-            modelAndView.getModelMap.put("types", typeDao.findAll().asJava)
+            modelAndView.getModelMap.put("types", typeDao.findAll().filterNot(_.deleted).sortBy(_.id).asJava)
     }
 }
