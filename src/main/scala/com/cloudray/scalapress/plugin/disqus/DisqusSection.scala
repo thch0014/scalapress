7682197e3a9c8af3c0bbba0d1c4e3153f54b2cd6
sksamuel <sam@sksamuel.com>
2013-06-21 13:09:49 +0100

    Added scalate. Moved disqus to scalate. Changed urls to be absolute

--- src/main/scala/com/cloudray/scalapress/plugin/disqus/DisqusSection.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/disqus/DisqusSection.scala
@@ -1,10 +1,11 @@
 package com.cloudray.scalapress.plugin.disqus
 
 import com.cloudray.scalapress.section.Section
-import javax.persistence.{Table, Entity}
+import javax.persistence.{Transient, Table, Entity}
 import com.cloudray.scalapress.ScalapressRequest
 import scala.beans.BeanProperty
 import com.cloudray.scalapress.util.UrlGenerator
+import org.fusesource.scalate.TemplateEngine
 
 /** @author Stephen Samuel */
 @Entity
@@ -16,29 +17,18 @@ class DisqusSection extends Section {
     override def backoffice: String = "/backoffice/plugin/disqus/section/" + id
     def desc: String = "Disqus commenting system"
 
+    @Transient val engine = new TemplateEngine
+
     def render(request: ScalapressRequest): Option[String] = {
 
         val title = request.obj.map(_.name).orElse(request.folder.map(_.name)).getOrElse("")
-        val url = request.obj.map(arg => UrlGenerator.url(arg)).orElse(request.folder.map(arg => UrlGenerator.url(arg))).getOrElse("")
+        val url = request.obj.map(arg => UrlGenerator.url(arg))
+          .orElse(request.folder.map(arg => UrlGenerator.url(arg)))
+          .map("http://" + request.installation.domain + _)
+          .getOrElse("")
 
-        Option(shortname).map(arg => {
-            s"""<div id="disqus_thread"></div>
-                <script type="text/javascript">
-                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                    var disqus_shortname = '$shortname';
-                    var disqus_identifier = '$id';
-                    var disqus_title = '$title';
-                    var disqus_url = '$url';
-                    /* * * DON'T EDIT BELOW THIS LINE * * */
-                    (function() {
-                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-                    })();
-                </script>
-                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-                <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-        """
-        })
+        Option(shortname)
+          .map(arg => engine.layout("/com/cloudray/scalapress/plugin/disqus/disqus.ssp",
+            Map("shortname" -> shortname, "title" -> title, "id" -> id, "url" -> url, "id" -> this.id.toString)))
     }
 }
