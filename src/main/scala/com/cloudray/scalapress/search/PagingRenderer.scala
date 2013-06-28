a5611af11514c5f0db76e056df4d429740f2b646
Nigel Horton <webdevelopment@clocked0ne.co.uk>
2013-06-28 11:41:10 +0100

    Pagination clearly described for users

--- src/main/scala/com/cloudray/scalapress/search/PagingRenderer.scala
+++ src/main/scala/com/cloudray/scalapress/search/PagingRenderer.scala
@@ -24,6 +24,7 @@ object PagingRenderer {
         val pages = _renderPages(paging, range)
         val xml = <div class="pagination">
             <ul>
+            	<li class="disabled"><i class="icon-search"></i> Viewing results on page </li>
                 {pages}
             </ul>
         </div>
