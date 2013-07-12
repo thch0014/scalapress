4286b04685445b1f6b68dd0f615f6c5e337fbed7
sksamuel <sam@sksamuel.com>
2013-07-12 16:31:34 +0100

    Fixed disqus quote handling

--- src/main/scala/com/cloudray/scalapress/plugin/disqus/DisqusSection.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/disqus/DisqusSection.scala
@@ -1,32 +1,33 @@
 package com.cloudray.scalapress.plugin.disqus
 
 import com.cloudray.scalapress.section.Section
-import javax.persistence.{Transient, Table, Entity}
+import javax.persistence.{Table, Entity}
 import com.cloudray.scalapress.ScalapressRequest
 import scala.beans.BeanProperty
 import com.cloudray.scalapress.util.UrlGenerator
-import org.fusesource.scalate.TemplateEngine
 
 /** @author Stephen Samuel */
 @Entity
 @Table(name = "plugin_disqus_section")
 class DisqusSection extends Section {
 
-    @BeanProperty var shortname: String = _
+  @BeanProperty var shortname: String = _
 
-    override def backoffice: String = "/backoffice/plugin/disqus/section/" + id
-    def desc: String = "Disqus commenting system"
+  override def backoffice: String = "/backoffice/plugin/disqus/section/" + id
+  def desc: String = "Disqus commenting system"
 
-    def render(request: ScalapressRequest): Option[String] = {
+  def render(request: ScalapressRequest): Option[String] = {
 
-        val title = request.obj.map(_.name).orElse(request.folder.map(_.name)).getOrElse("")
-        val url = request.obj.map(arg => UrlGenerator.url(arg))
-          .orElse(request.folder.map(arg => UrlGenerator.url(arg)))
-          .map("http://" + request.installation.domain + _)
-          .getOrElse("")
+    val title = request.obj.map(_.name).orElse(request.folder.map(_.name)).getOrElse("")
+    val url = request.obj.map(arg => UrlGenerator.url(arg))
+      .orElse(request.folder.map(arg => UrlGenerator.url(arg)))
+      .map("http://" + request.installation.domain + _)
+      .getOrElse("")
 
-        Option(shortname)
-          .map(arg => engine.layout("/com/cloudray/scalapress/plugin/disqus/disqus.ssp",
-            Map("shortname" -> shortname, "title" -> title, "id" -> id, "url" -> url, "id" -> this.id.toString)))
-    }
+    val normalizedTitle = title.replace("\"", "'")
+
+    Option(shortname)
+      .map(arg => engine.layout("/com/cloudray/scalapress/plugin/disqus/disqus.ssp",
+      Map("shortname" -> shortname, "title" -> normalizedTitle, "id" -> id, "url" -> url, "id" -> this.id.toString)))
+  }
 }
--- src/test/scala/com/cloudray/scalapress/plugin/disqus/DisqusSectionTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/disqus/DisqusSectionTest.scala
@@ -11,66 +11,75 @@ import com.cloudray.scalapress.settings.Installation
 /** @author Stephen Samuel */
 class DisqusSectionTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    val section = new DisqusSection
-    section.id = 152
-    section.shortname = "sammythebull"
+  val section = new DisqusSection
+  section.id = 152
+  section.shortname = "sammythebull"
 
-    val obj = new Obj
-    obj.name = "coldplay tickets"
+  val obj = new Obj
+  obj.id = 34
+  obj.name = "coldplay tickets"
 
-    val folder = new Folder
-    folder.name = "live events"
+  val folder = new Folder
+  folder.name = "live events"
 
-    val req = mock[ScalapressRequest]
-    val installation = mock[Installation]
-    Mockito.when(req.installation).thenReturn(installation)
-    Mockito.when(installation.domain).thenReturn("buffet.com")
+  val req = mock[ScalapressRequest]
+  val installation = mock[Installation]
+  Mockito.when(req.installation).thenReturn(installation)
+  Mockito.when(installation.domain).thenReturn("buffet.com")
 
-    test("disqus render includes shortname") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_shortname = 'sammythebull';")
-    }
+  test("disqus render includes shortname") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_shortname = "sammythebull";"""))
+  }
 
-    test("disqus render uses section id for disqus identifer") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_identifier = '152';")
-    }
+  test("disqus render uses section and object id for disqus identifer") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_identifier = "152";"""))
+  }
 
-    test("given an object request then the page title is the name of the object") {
-        Mockito.when(req.obj).thenReturn(Option(obj))
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_title = 'http://buffet.com/coldplay-tickets';")
-    }
+  test("given an object request then the page title is the name of the object") {
+    Mockito.when(req.obj).thenReturn(Option(obj))
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_title = "coldplay tickets";"""))
+  }
 
-    test("given a folder request then the page title is the name of the folder") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(Option(folder))
-        section.render(req).get.contains("var disqus_title = 'http://buffet.com/live-events';")
-    }
+  test("given a folder request then the page title is the name of the folder") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(Option(folder))
+    assert(section.render(req).get.contains( """var disqus_title = "live events";"""))
+  }
 
-    test("given an object request then the page url is the url of the object") {
-        Mockito.when(req.obj).thenReturn(Option(obj))
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_url = 'coldplay tickets';")
-    }
+  test("given an object request then the page url is the url of the object") {
+    Mockito.when(req.obj).thenReturn(Option(obj))
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_url = "http://buffet.com/object-34-coldplay-tickets";"""))
+  }
 
-    test("given a folder request then the page url is the url of the folder") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(Option(folder))
-        section.render(req).get.contains("var disqus_url = 'live events';")
-    }
+  test("given a folder request then the page url is the url of the folder") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(Option(folder))
+    assert(section.render(req).get.contains( """var disqus_url = "http://buffet.com/folder-0-live-events";"""))
+  }
 
-    test("given a non object/folder request then the page url is the empty string") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_url = '';")
-    }
+  test("given a non object/folder request then the page url is the empty string") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_url = "";"""))
+  }
 
-    test("given a non object/folder request then the page title is the empty string") {
-        Mockito.when(req.obj).thenReturn(None)
-        Mockito.when(req.folder).thenReturn(None)
-        section.render(req).get.contains("var disqus_title = '';")
-    }
+  test("given a non object/folder request then the page title is the empty string") {
+    Mockito.when(req.obj).thenReturn(None)
+    Mockito.when(req.folder).thenReturn(None)
+    assert(section.render(req).get.contains( """var disqus_title = "";"""))
+  }
+
+  test("an object's title should remove quote marks") {
+    obj.name = "coldplay are \"great\""
+    Mockito.when(req.obj).thenReturn(Option(obj))
+    Mockito.when(req.folder).thenReturn(None)
+    println(section.render(req))
+    assert(section.render(req).get.contains( """var disqus_title = "coldplay are 'great'";"""))
+  }
 }
