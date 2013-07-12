f1384b8787ad14fe9d1252a8e604f88ba791cfe1
sksamuel <sam@sksamuel.com>
2013-07-12 13:06:37 +0100

    Fixed dimension redirect

--- src/main/scala/com/cloudray/scalapress/plugin/variations/DimensionEditController.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/variations/DimensionEditController.scala
@@ -3,7 +3,6 @@ package com.cloudray.scalapress.plugin.variations
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation._
 import org.springframework.beans.factory.annotation.Autowired
-import com.cloudray.scalapress.ScalapressContext
 
 /** @author Stephen Samuel */
 @Controller
@@ -11,7 +10,6 @@ import com.cloudray.scalapress.ScalapressContext
 class DimensionEditController {
 
   @Autowired var dimensionDao: DimensionDao = _
-  @Autowired var context: ScalapressContext = _
 
   @RequestMapping(method = Array(RequestMethod.GET))
   def edit = "admin/plugin/variations/dimensions/edit.vm"
@@ -19,7 +17,7 @@ class DimensionEditController {
   @RequestMapping(method = Array(RequestMethod.POST))
   def save(@ModelAttribute("dimension") dimension: Dimension) = {
     dimensionDao.save(dimension)
-    "redirect:/backoffice/plugin/variations/dimensions"
+    "redirect:/backoffice/plugin/variations/dimensions?objectTypeId=" + dimension.objectType.id
   }
 
   @ModelAttribute("dimension") def dimension(@PathVariable("id") id: Long) = dimensionDao.find(id)
---
+++ src/test/scala/com/cloudray/scalapress/plugin/variations/DimensionEditControllerTest.scala
@@ -0,0 +1,25 @@
+package com.cloudray.scalapress.plugin.variations
+
+import org.scalatest.{OneInstancePerTest, FlatSpec}
+import org.scalatest.mock.MockitoSugar
+import com.cloudray.scalapress.obj.{Obj, ObjectType}
+
+/** @author Stephen Samuel */
+class DimensionEditControllerTest extends FlatSpec with MockitoSugar with OneInstancePerTest {
+
+  val controller = new DimensionEditController
+  controller.dimensionDao = mock[DimensionDao]
+
+  val d1 = new Dimension
+  d1.objectType = new ObjectType
+  d1.objectType.id = 54
+
+  val obj = new Obj
+  obj.id = 1
+  obj.objectType = d1.objectType
+
+  "a dimension edit controller" should "redirect to the dimensions list page after saving" in {
+    val redirect = controller.save(d1)
+    assert("redirect:/backoffice/plugin/variations/dimensions?objectTypeId=54" === redirect)
+  }
+}
