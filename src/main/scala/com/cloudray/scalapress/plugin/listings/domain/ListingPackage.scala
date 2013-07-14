c61277e52565fa33e425b2cd44ecdb5fff8de672
sksamuel <sam@sksamuel.com>
2013-07-14 19:09:28 +0100

    Updated listing package to use longer field size

--- src/main/scala/com/cloudray/scalapress/plugin/listings/domain/ListingPackage.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/listings/domain/ListingPackage.scala
@@ -10,50 +10,52 @@ import scala.beans.BeanProperty
 @Table(name = "listings_packages")
 class ListingPackage {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @BeanProperty var id: Long = _
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @BeanProperty var id: Long = _
 
-    @Column(name = "maxcategories")
-    @BeanProperty var maxFolders: Int = _
+  @Column(name = "maxcategories")
+  @BeanProperty var maxFolders: Int = _
 
-    @ManyToOne
-    @BeanProperty var objectType: ObjectType = _
+  @ManyToOne
+  @BeanProperty var objectType: ObjectType = _
 
-    @BeanProperty var maxImages: Int = _
+  @BeanProperty var maxImages: Int = _
 
-    @BeanProperty var description: String = _
+  @Column(name = "description", length = 3000)
+  @BeanProperty var description: String = _
 
-    @BeanProperty var maxCharacters: Int = _
+  @BeanProperty var maxCharacters: Int = _
 
-    @BeanProperty var deleted: Boolean = _
+  @BeanProperty var deleted: Boolean = _
 
-    @BeanProperty var autoPublish: Boolean = _
+  @BeanProperty var autoPublish: Boolean = _
 
-    @BeanProperty var labels: String = _
+  @Column(name = "description", length = 3000)
+  @BeanProperty var labels: String = _
 
-    @Column(length = 10000)
-    @BeanProperty var name: String = _
+  @Column(length = 3000)
+  @BeanProperty var name: String = _
 
-    @BeanProperty var fee: Int = _
+  @BeanProperty var fee: Int = _
 
-    @BeanProperty var duration: Int = _
+  @BeanProperty var duration: Int = _
 
-    @BeanProperty var folders: String = _
+  @BeanProperty var folders: String = _
 
-    def priceText = {
-        val buffer = new ArrayBuffer[String]
-        buffer.append(fee match {
-            case 0 => "Free"
-            case _ => "£" + "%.2f".format(fee / 100.0)
-        })
-        if (duration > 0)
-            buffer.append(duration match {
-                case x if x == 365 => "for 1 year"
-                case x if x == 30 => "for 1 month"
-                case x if (x % 30 == 0) => "for " + duration / 30 + " months"
-                case x => "for " + x + " days"
-            })
-        buffer.mkString(" ")
-    }
+  def priceText = {
+    val buffer = new ArrayBuffer[String]
+    buffer.append(fee match {
+      case 0 => "Free"
+      case _ => "£" + "%.2f".format(fee / 100.0)
+    })
+    if (duration > 0)
+      buffer.append(duration match {
+        case x if x == 365 => "for 1 year"
+        case x if x == 30 => "for 1 month"
+        case x if (x % 30 == 0) => "for " + duration / 30 + " months"
+        case x => "for " + x + " days"
+      })
+    buffer.mkString(" ")
+  }
 }
--- src/test/scala/com/cloudray/scalapress/obj/ObjectExporterTest.scala
+++ src/test/scala/com/cloudray/scalapress/obj/ObjectExporterTest.scala
@@ -7,84 +7,84 @@ import com.cloudray.scalapress.obj.attr.{AttributeValue, Attribute}
 /** @author Stephen Samuel */
 class ObjectExporterTest extends FunSuite with OneInstancePerTest with MockitoSugar {
 
-    val objectType = new ObjectType
-    val attr1 = new Attribute
-    attr1.name = "brand"
-    attr1.id = 12
+  val objectType = new ObjectType
+  val attr1 = new Attribute
+  attr1.name = "brand"
+  attr1.id = 12
 
-    val attr2 = new Attribute
-    attr2.name = "code"
-    attr2.id = 87
+  val attr2 = new Attribute
+  attr2.name = "code"
+  attr2.id = 87
 
-    val attributes = List(attr1, attr2)
+  val attributes = List(attr1, attr2)
 
-    val obj = new Obj
-    obj.name = "coldplay tickets"
-    obj.id = 123
-    obj.dateCreated = 1365722181000l
-    obj.price = 599
-    obj.costPrice = 450
-    obj.status = "super status"
-    obj.stock = 5
-    obj.rrp = 1999
-    obj.vatRate = 10.00
+  val obj = new Obj
+  obj.name = "coldplay tickets"
+  obj.id = 123
+  obj.dateCreated = 1365722181000l
+  obj.price = 599
+  obj.costPrice = 450
+  obj.status = "super status"
+  obj.stock = 5
+  obj.rrp = 1999
+  obj.vatRate = 10.00
 
-    val av1 = new AttributeValue
-    av1.attribute = attr1
-    av1.value = "Samsung"
+  val av1 = new AttributeValue
+  av1.attribute = attr1
+  av1.value = "Samsung"
 
-    val av2 = new AttributeValue
-    av2.attribute = attr2
-    av2.value = "GalaxyS"
+  val av2 = new AttributeValue
+  av2.attribute = attr2
+  av2.value = "GalaxyS"
 
-    obj.attributeValues.add(av1)
-    obj.attributeValues.add(av2)
+  obj.attributeValues.add(av1)
+  obj.attributeValues.add(av2)
 
-    test("header happy path") {
+  test("header happy path") {
 
-        val header = new ObjectExporter()._header(attributes)
-        assert(Array("id",
-            "date",
-            "name",
-            "status",
-            "url",
-            "price",
-            "vat rate",
-            "price inc",
-            "rrp",
-            "cost",
-            "profit",
-            "stock",
-            "brand",
-            "code") === header)
-    }
+    val header = new ObjectExporter()._header(attributes)
+    assert(Array("id",
+      "date",
+      "name",
+      "status",
+      "url",
+      "price",
+      "vat rate",
+      "price inc",
+      "rrp",
+      "cost",
+      "profit",
+      "stock",
+      "brand",
+      "code") === header)
+  }
 
-    test("row happy path") {
+  test("row happy path") {
 
-        val row = new ObjectExporter()._row(obj, attributes, "mysite.com")
-        assert(Array[String]("123",
-            "11-04-2013",
-            "coldplay tickets",
-            "super status",
-            "http://mysite.com/object-123-coldplay-tickets", "5.99", "10.0", "6.58", "19.99", "4.50", "1.49", "5",
-            "Samsung",
-            "GalaxyS") === row)
-    }
+    val row = new ObjectExporter()._row(obj, attributes, "mysite.com")
+    assert(Array[String]("123",
+      "11-04-2013",
+      "coldplay tickets",
+      "super status",
+      "http://mysite.com/object-123-coldplay-tickets", "5.99", "10.0", "6.58", "19.99", "4.50", "1.49", "5",
+      "Samsung",
+      "GalaxyS") === row)
+  }
 
-    test("row handles zeros for pricing") {
+  test("row handles zeros for pricing") {
 
-        obj.price = 0
-        obj.costPrice = 0
-        obj.stock = 0
-        obj.rrp = 0
+    obj.price = 0
+    obj.costPrice = 0
+    obj.stock = 0
+    obj.rrp = 0
 
-        val header = new ObjectExporter()._row(obj, attributes, "mysite.com")
-        assert(Array[String]("123",
-            "11-04-2013",
-            "coldplay tickets", "super status",
-            "http://mysite.com/object-123-coldplay-tickets", "0.00", "10.0", "0.00", "0.00", "0.00", "0.00", "0",
-            "Samsung",
-            "GalaxyS") === header)
-    }
+    val header = new ObjectExporter()._row(obj, attributes, "mysite.com")
+    assert(Array[String]("123",
+      "11-04-2013",
+      "coldplay tickets", "super status",
+      "http://mysite.com/object-123-coldplay-tickets", "0.00", "10.0", "0.00", "0.00", "0.00", "0.00", "0",
+      "Samsung",
+      "GalaxyS") === header)
+  }
 }
 
