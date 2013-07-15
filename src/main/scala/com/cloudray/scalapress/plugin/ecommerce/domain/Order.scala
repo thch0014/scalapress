7a8f2588169db6440de5c6f71a36a8fffd986b8e
sksamuel <sam@sksamuel.com>
2013-07-15 01:30:50 +0100

    Added daily sales totals but commented out.

--- src/main/scala/com/cloudray/scalapress/plugin/ecommerce/domain/Order.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/ecommerce/domain/Order.scala
@@ -16,89 +16,89 @@ import javax.persistence.CascadeType
 @Table(name = "orders")
 class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @BeanProperty var id: Long = _
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @BeanProperty var id: Long = _
 
-    @BeanProperty var status: String = _
+  @BeanProperty var status: String = _
 
-    @BeanProperty var deliveryCharge: Int = _
-    @BeanProperty var deliveryVatRate: Double = _
+  @BeanProperty var deliveryCharge: Int = _
+  @BeanProperty var deliveryVatRate: Double = _
 
-    @BeanProperty var customerNote: String = _
+  @BeanProperty var customerNote: String = _
 
-    def deliveryEx: Double = deliveryCharge / 100.0
-    def deliveryVat: Double = if (vatable) deliveryEx * deliveryVatRate / 100.0 else 0
-    def deliveryInc: Double = deliveryEx + deliveryVat
+  def deliveryEx: Double = deliveryCharge / 100.0
+  def deliveryVat: Double = if (vatable) deliveryEx * deliveryVatRate / 100.0 else 0
+  def deliveryInc: Double = deliveryEx + deliveryVat
 
-    @Column(name = "deliveryDetails")
-    @BeanProperty var deliveryDetails: String = _
+  @Column(name = "deliveryDetails")
+  @BeanProperty var deliveryDetails: String = _
 
-    @Column(name = "datePlaced")
-    @BeanProperty var datePlaced: Long = _
-    def datePlacedLondon(): DateTime = new DateTime(datePlaced, DateTimeZone.forID("Europe/London"))
+  @Column(name = "datePlaced")
+  @BeanProperty var datePlaced: Long = _
+  def datePlacedLondon(): DateTime = new DateTime(datePlaced, DateTimeZone.forID("Europe/London"))
 
-    @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL))
-    @BeanProperty var payments: java.util.Set[Transaction] = new java.util.HashSet[Transaction]()
+  @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL))
+  @BeanProperty var payments: java.util.Set[Transaction] = new java.util.HashSet[Transaction]()
 
-    @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL), orphanRemoval = true)
-    @Fetch(FetchMode.SUBSELECT)
-    @BatchSize(size = 40)
-    @BeanProperty var lines: java.util.Set[OrderLine] = new java.util.HashSet[OrderLine]()
-    def sortedLines: Seq[OrderLine] = lines.asScala.toSeq.sortBy(_.id)
-    def sortedLinesJava = sortedLines.asJava
+  @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL), orphanRemoval = true)
+  @Fetch(FetchMode.SUBSELECT)
+  @BatchSize(size = 50)
+  @BeanProperty var lines: java.util.Set[OrderLine] = new java.util.HashSet[OrderLine]()
+  def sortedLines: Seq[OrderLine] = lines.asScala.toSeq.sortBy(_.id)
+  def sortedLinesJava = sortedLines.asJava
 
-    @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL), orphanRemoval = true)
-    @BeanProperty var comments: java.util.Set[OrderComment] = new java.util.HashSet[OrderComment]()
-    def sortedComments: Seq[OrderComment] = comments.asScala.toSeq.sortBy(_.id)
-    def sortedCommentsJava = sortedComments.asJava
+  @OneToMany(mappedBy = "order", cascade = Array(CascadeType.ALL), orphanRemoval = true)
+  @BeanProperty var comments: java.util.Set[OrderComment] = new java.util.HashSet[OrderComment]()
+  def sortedComments: Seq[OrderComment] = comments.asScala.toSeq.sortBy(_.id)
+  def sortedCommentsJava = sortedComments.asJava
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "deliveryAddress")
-    @BeanProperty var deliveryAddress: Address = _
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "deliveryAddress")
+  @BeanProperty var deliveryAddress: Address = _
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "billingAddress")
-    @BeanProperty var billingAddress: Address = _
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "billingAddress")
+  @BeanProperty var billingAddress: Address = _
 
-    @BeanProperty var ipAddress: String = _
+  @BeanProperty var ipAddress: String = _
 
-    @BeanProperty var vatable: Boolean = _
+  @BeanProperty var vatable: Boolean = _
 
-    @BeanProperty var customerReference: String = _
+  @BeanProperty var customerReference: String = _
 
-    @BeanProperty var reference: String = _
+  @BeanProperty var reference: String = _
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account")
-    @BeanProperty var account: Obj = _
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "account")
+  @BeanProperty var account: Obj = _
 
-    @Column(name = "salesPerson")
-    @BeanProperty var createdBy: Long = _
+  @Column(name = "salesPerson")
+  @BeanProperty var createdBy: Long = _
 
-    def linesSubtotal: Double = lines.asScala.toSeq.map(_.totalExVat).foldLeft(0.0)((a, b) => a + b)
-    def linesVat: Double = if (vatable) lines.asScala.toSeq.map(_.totalVat).foldLeft(0.0)((a, b) => a + b) else 0.0
-    def linesTotal: Double = lines.asScala.toSeq.map(_.totalIncVat).foldLeft(0.0)((a, b) => a + b)
+  def linesSubtotal: Double = lines.asScala.toSeq.map(_.totalExVat).foldLeft(0.0)((a, b) => a + b)
+  def linesVat: Double = if (vatable) lines.asScala.toSeq.map(_.totalVat).foldLeft(0.0)((a, b) => a + b) else 0.0
+  def linesTotal: Double = lines.asScala.toSeq.map(_.totalIncVat).foldLeft(0.0)((a, b) => a + b)
 
-    def vat: Double = if (vatable) linesVat + deliveryVat else 0.0
-    def subtotal: Double = linesSubtotal + deliveryEx
-    def total: Double = ((linesTotal + deliveryInc) * 100).toInt / 100.0
+  def vat: Double = if (vatable) linesVat + deliveryVat else 0.0
+  def subtotal: Double = linesSubtotal + deliveryEx
+  def total: Double = ((linesTotal + deliveryInc) * 100).toInt / 100.0
 }
 
 object Order {
 
-    val STATUS_NEW = "New"
-    val STATUS_PAID = "Paid"
-    val STATUS_COMPLETED = "Completed"
-    val STATUS_CANCELLED = "Cancelled"
+  val STATUS_NEW = "New"
+  val STATUS_PAID = "Paid"
+  val STATUS_COMPLETED = "Completed"
+  val STATUS_CANCELLED = "Cancelled"
 
-    def apply(ipAddress: String, account: Obj) = {
-        val order = new Order
-        order.datePlaced = new DateTime(DateTimeZone.UTC).getMillis
-        order.status = "New"
-        order.ipAddress = ipAddress
-        order.vatable = true
-        order.account = account
-        order
-    }
+  def apply(ipAddress: String, account: Obj) = {
+    val order = new Order
+    order.datePlaced = new DateTime(DateTimeZone.UTC).getMillis
+    order.status = "New"
+    order.ipAddress = ipAddress
+    order.vatable = true
+    order.account = account
+    order
+  }
 }
--- src/main/scala/com/cloudray/scalapress/util/mvc/DashboardController.scala
+++ src/main/scala/com/cloudray/scalapress/util/mvc/DashboardController.scala
@@ -16,22 +16,34 @@ import com.cloudray.scalapress.obj.Obj
 @RequestMapping(Array("backoffice"))
 class DashboardController {
 
-    val in = getClass.getResourceAsStream("/buildNumber.properties")
-    val props = new Properties()
-    props.load(in)
-    IOUtils.closeQuietly(in)
+  val in = getClass.getResourceAsStream("/buildNumber.properties")
+  val props = new Properties()
+  props.load(in)
+  IOUtils.closeQuietly(in)
 
-    @Autowired var context: ScalapressContext = _
+  @Autowired var context: ScalapressContext = _
 
-    @RequestMapping(produces = Array("text/html"))
-    def dashboard = "admin/dashboard.vm"
+  @RequestMapping(produces = Array("text/html"))
+  def dashboard = "admin/dashboard.vm"
 
-    @ModelAttribute("installation") def installation = context.installationDao.get
-    @ModelAttribute("indexed") def indexed = context.searchService.count
-    @ModelAttribute("user") def indexed(req: HttpServletRequest) = SpringSecurityResolver.getAdminDetails(req).user
-    @ModelAttribute("folderCount") def folderCount = context.folderDao.findAll().size
+  //  @ModelAttribute("dailySales") def dailySales: java.util.Map[String, Int] = {
+  //    val q = new OrderQuery
+  //    q.pageSize = 100000000
+  //    q.from = Some(new DateMidnight(DateTimeZone.UTC).minusDays(7).getMillis)
+  //    q.to = Some(new DateMidnight(DateTimeZone.UTC).plusDays(1).getMillis)
+  //    val orders = context.bean[OrderDao].search(q).results
+  //    val grouped = orders.groupBy(order => new DateMidnight(order.datePlaced, DateTimeZone.UTC))
+  //    val totals = grouped
+  //      .map(group => (group._1.toString("dd/MM/yyyy"), group._2.foldLeft(0)((a, b) => a + b.total.toInt)))
+  //    totals.asJava
+  //  }
 
-    @ModelAttribute("recentObjects") def recentObjects: java.util.List[Obj] = context.objectDao.recent(8).asJava
+  @ModelAttribute("installation") def installation = context.installationDao.get
+  @ModelAttribute("indexed") def indexed = context.searchService.count
+  @ModelAttribute("user") def indexed(req: HttpServletRequest) = SpringSecurityResolver.getAdminDetails(req).user
+  @ModelAttribute("folderCount") def folderCount = context.folderDao.findAll().size
 
-    @ModelAttribute("buildNumber") def buildNumber = props.get("buildNumber")
+  @ModelAttribute("recentObjects") def recentObjects: java.util.List[Obj] = context.objectDao.recent(8).asJava
+
+  @ModelAttribute("buildNumber") def buildNumber = props.get("buildNumber")
 }
