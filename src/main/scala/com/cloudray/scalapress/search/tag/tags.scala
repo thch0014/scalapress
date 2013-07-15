7a931050637a6da60b26ea75b1c0fbb46aca3e91
sksamuel <sam@sksamuel.com>
2013-07-15 08:48:51 +0100

    Fixed attribute search tag

--- src/main/scala/com/cloudray/scalapress/search/tag/tags.scala
+++ src/main/scala/com/cloudray/scalapress/search/tag/tags.scala
@@ -4,55 +4,63 @@ import com.cloudray.scalapress.{Tag, ScalapressRequest}
 import scala.collection.JavaConverters._
 import com.cloudray.scalapress.obj.attr.AttributeOption
 import com.cloudray.scalapress.theme.tag.ScalapressTag
+import scala.xml.Utility
+import com.cloudray.scalapress.enums.AttributeType
 
 /** @author Stephen Samuel */
 object SearchFormTag extends ScalapressTag {
-    def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = None
+  def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = None
 }
 
 @Tag("quicksearch")
 class QuickSearchTag extends ScalapressTag {
 
-    def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+  def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
 
-        val placeholder = params.get("placeholder").orElse(params.get("initial")).getOrElse("")
-        val objectType = params
-          .get("objectType")
-          .map(arg => <input type="hidden" value={arg.toString} name="objectType"/>).orNull
+    val placeholder = params.get("placeholder").orElse(params.get("initial")).getOrElse("")
+    val objectType = params
+      .get("objectType")
+      .map(arg => <input type="hidden" value={arg.toString} name="objectType"/>).orNull
 
-        Some(<form method="GET" action="/search">
-            {objectType}<input type="text" name="q" class="search-query" placeholder={placeholder}/>
-            <button>Go</button>
-        </form>.toString())
-    }
+    Some(<form method="GET" action="/search">
+      {objectType}<input type="text" name="q" class="search-query" placeholder={placeholder}/>
+      <button>Go</button>
+    </form>.toString())
+  }
 }
 
 @Tag("search_attribute")
 class AttributeSearchTag extends ScalapressTag {
 
-    def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+  def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
 
-        params.get(" id ") match {
-            case None => None
-            case Some(id) =>
-                val attribute = request.context.attributeDao.find(id.toLong)
-                val xml =
-                    <form method=" GET " action="/search">
-                        <select name="q" action="/search">
-                            {options(attribute.options.asScala)}
-                        </select>
-                        <button>Go</button>
-                    </form>
-                Some(xml.toString())
-        }
-    }
-
-    private def options(options: Seq[AttributeOption]) = {
-        options.filter(_.value != null).sortBy(_.value).map(opt => {
-            <option>
-                {opt.value}
-            </option>
+    params.get("id") match {
+      case None => None
+      case Some(id) =>
+        val name = "attr_" + id
+        Option(request.context.attributeDao.find(id.toLong)).flatMap(attr => {
+          attr.attributeType match {
+            case AttributeType.Selection =>
+              val xml =
+                <form method="GET" action="/search">
+                  <select name={name} action="/search">
+                    {options(attr.options.asScala)}
+                  </select>
+                  <button>Go</button>
+                </form>
+              Some(Utility.trim(xml).toString())
+            case _ => None
+          }
         })
     }
+  }
+
+  private def options(options: Seq[AttributeOption]) = {
+    options.filter(_.value != null).sortBy(_.value).map(opt => {
+      <option>
+        {opt.value}
+      </option>
+    })
+  }
 }
 
---
+++ src/test/scala/com/cloudray/scalapress/search/tag/AttributeSearchTagTest.scala
@@ -0,0 +1,64 @@
+package com.cloudray.scalapress.search.tag
+
+import org.scalatest.{OneInstancePerTest, FlatSpec}
+import org.scalatest.mock.MockitoSugar
+import com.cloudray.scalapress.{ScalapressRequest, ScalapressContext}
+import javax.servlet.http.HttpServletRequest
+import com.cloudray.scalapress.obj.attr.{AttributeOption, Attribute, AttributeDao}
+import org.mockito.Mockito
+import com.cloudray.scalapress.enums.AttributeType
+
+/** @author Stephen Samuel */
+class AttributeSearchTagTest extends FlatSpec with MockitoSugar with OneInstancePerTest {
+
+  val context = new ScalapressContext()
+  context.attributeDao = mock[AttributeDao]
+  val req = mock[HttpServletRequest]
+
+  val sreq = ScalapressRequest(req, context)
+
+  val tag = new AttributeSearchTag
+
+  "an attribute search tag" should "render using the id param" in {
+
+    val option = new AttributeOption
+    option.value = "coldplay"
+
+    val a = new Attribute
+    a.attributeType = AttributeType.Selection
+    a.options.add(option)
+    Mockito.when(context.attributeDao.find(9)).thenReturn(a)
+
+    val actual = tag.render(sreq, Map("id" -> "9"))
+    assert(
+      """<form method="GET" action="/search"><select name="attr_9" action="/search"><option>coldplay</option></select><button>Go</button></form>""" === actual
+        .get)
+  }
+
+  it should "render None when the attribute is not an attribute Selection type" in {
+
+    val option = new AttributeOption
+    option.value = "coldplay"
+
+    val a = new Attribute
+    a.attributeType = AttributeType.Text
+    a.options.add(option)
+    Mockito.when(context.attributeDao.find(9)).thenReturn(a)
+
+    val actual = tag.render(sreq, Map("id" -> "9"))
+    assert(actual.isEmpty)
+  }
+
+  it should "render None when the attribute id is not valid" in {
+
+    val option = new AttributeOption
+    option.value = "coldplay"
+
+    val a = new Attribute
+    a.options.add(option)
+    Mockito.when(context.attributeDao.find(9)).thenReturn(a)
+
+    val actual = tag.render(sreq, Map("id" -> "149"))
+    assert(actual.isEmpty)
+  }
+}
