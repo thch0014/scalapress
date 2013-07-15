d580f48873e36c6e8bb19b89920e9a633d39d7d0
sksamuel <sam@sksamuel.com>
2013-07-15 11:09:50 +0100

    Moved colorbox to SSP and Plugin

--- src/main/scala/com/cloudray/scalapress/obj/tag/images.scala
+++ src/main/scala/com/cloudray/scalapress/obj/tag/images.scala
@@ -10,91 +10,55 @@ import com.cloudray.scalapress.util.UrlGenerator
 
 object ImagesTag extends ScalapressTag with TagBuilder with Logging {
 
-    def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+  def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
 
-        val h = params.get("h").orElse(params.get("height")).getOrElse("0").toInt
-        val w = params.get("w").orElse(params.get("width")).getOrElse("0").toInt
-        val limit = params.get("limit").getOrElse("100").toInt
-	val cssImgClass = params.get("imgclass").getOrElse("")
+    val h = params.get("h").orElse(params.get("height")).getOrElse("0").toInt
+    val w = params.get("w").orElse(params.get("width")).getOrElse("0").toInt
+    val limit = params.get("limit").getOrElse("100").toInt
+    val cssImgClass = params.get("imgclass").getOrElse("")
 
-        val obj = request.obj.orElse(request.line.map(_.obj))
-        obj match {
-            case None => None
-            case Some(o) =>
-                val images = o.images.asScala.toSeq
-                val sorted = images.sortBy(_.position)
-                val rendered = sorted.take(limit).map(i => {
-                    val src = if (w == 0 || h == 0)
-                        request.context.assetStore.link(i.filename)
-                    else
-                        request.context.imageService.imageLink(i.filename, w, h)
+    val obj = request.obj.orElse(request.line.map(_.obj))
+    obj match {
+      case None => None
+      case Some(o) =>
+        val images = o.images.asScala.toSeq
+        val sorted = images.sortBy(_.position)
+        val rendered = sorted.take(limit).map(i => {
+          val src = if (w == 0 || h == 0)
+            request.context.assetStore.link(i.filename)
+          else
+            request.context.imageService.imageLink(i.filename, w, h)
 
-                    val html = if (w == 0 || h == 0)
-                            <img src={src} class={cssImgClass}/>.toString()
-                    else
-                            <img src={src} height={h.toString} width={w.toString} class={cssImgClass}/>.toString()
+          val html = if (w == 0 || h == 0)
+              <img src={src} class={cssImgClass}/>.toString()
+          else
+              <img src={src} height={h.toString} width={w.toString} class={cssImgClass}/>.toString()
 
-                    params.get("link") match {
+          params.get("link") match {
 
-                        case Some(link) if link == "image" =>
-                            buildLink(request.context.assetStore.link(i.filename), html, params)
+            case Some(link) if link == "image" =>
+              buildLink(request.context.assetStore.link(i.filename), html, params)
 
-                        case Some(link) if link == "object" =>
-                            buildLink(UrlGenerator.url(o), html, params)
+            case Some(link) if link == "object" =>
+              buildLink(UrlGenerator.url(o), html, params)
 
-                        case _ => build(html, params)
-                    }
-                })
+            case _ => build(html, params)
+          }
+        })
 
-                Option(rendered.mkString("\n"))
-        }
+        Option(rendered.mkString("\n"))
     }
+  }
 }
 
 object ImageUrlTag extends ScalapressTag with TagBuilder {
 
-    override def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+  override def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
 
-        request.obj.map(obj => {
-            val limit = params.get("limit").getOrElse("1").toInt
-            val rendered = obj.images.asScala.toSeq.sortBy(_.id).take(limit).map(i => "/images/" + i.filename)
-            rendered.mkString("\n")
-        })
-    }
-}
-
-@Tag("colorbox")
-class ColorboxTag extends ScalapressTag with TagBuilder {
-    override def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
-
-        val height = params.get("height").getOrElse("120").toInt
-        val width = params.get("width").getOrElse("160").toInt
-        val text = params.get("text").getOrElse("")
-        request.obj.map(obj => {
-
-            var count = 0
-            val images = obj.images.asScala.toSeq
-            val sorted = images.sortBy(_.position)
-            val rendered = sorted.map(image => {
-
-                val original = request.context.assetStore.link(image.filename)
-                val thumb = request.context.imageService.imageLink(image.filename, width, height)
-                val display = if (count == 0) "" else "display: none"
-                count = count + 1
-
-                "<a class='colorboxgroup' href='" + original + "' title='" + obj
-                  .name + "' style='" + display + "'><img src='" + thumb + "' width='" + width
-                  .toString + "' height='" + height + "'/><span>" + text + "</span></a> "
-
-            }).mkString("\n")
-
-            rendered +
-              """<script>
-                 $(document).ready(function() {
-                    $(".colorboxgroup").colorbox({ rel: 'colorboxgroup', top: '150px' });
-                 });
-                </script>"""
-        })
-
-    }
+    request.obj.map(obj => {
+      val limit = params.get("limit").getOrElse("1").toInt
+      val rendered = obj.images.asScala.toSeq.sortBy(_.id).take(limit).map(i => "/images/" + i.filename)
+      rendered.mkString("\n")
+    })
+  }
 }
---
+++ src/main/scala/com/cloudray/scalapress/plugin/lightbox/colorbox/ColorboxTag.scala
@@ -0,0 +1,41 @@
+package com.cloudray.scalapress.plugin.lightbox.colorbox
+
+import com.cloudray.scalapress.{ScalapressRequest, Tag}
+import com.cloudray.scalapress.theme.tag.{TagBuilder, ScalapressTag}
+import scala.collection.JavaConverters._
+import org.fusesource.scalate.TemplateEngine
+
+/** @author Stephen Samuel */
+@Tag("colorbox")
+class ColorboxTag extends ScalapressTag with TagBuilder {
+
+  val engine = new TemplateEngine
+
+  override def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+
+    val height = params.get("height").getOrElse("120").toInt
+    val width = params.get("width").getOrElse("160").toInt
+    val text = params.get("text").getOrElse("")
+    request.obj.map(obj => {
+
+      var count = 0
+      val images = obj.images.asScala.toSeq
+      val sorted = images.sortBy(_.position)
+      val links = sorted.map(image => {
+
+        val original = request.context.assetStore.link(image.filename)
+        val thumb = request.context.imageService.imageLink(image.filename, width, height)
+        val style = if (count == 0) "" else "display: none"
+        count = count + 1
+
+        engine.layout("/com/cloudray/scalapress/plugin/lightbox/colorbox/image.ssp",
+          Map("original" -> original, "thumb" -> thumb, "style" -> style, "title" -> obj.name,
+            "width" -> width, "height" -> height, "text" -> text))
+
+      })
+
+      val script = engine.layout("/com/cloudray/scalapress/plugin/lightbox/colorbox/script.ssp")
+      links.mkString("\n\n") + script
+    })
+  }
+}
