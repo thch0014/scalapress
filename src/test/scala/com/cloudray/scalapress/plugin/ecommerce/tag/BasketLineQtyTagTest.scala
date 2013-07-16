9f3c3114b23d52b7ae816dd710633a1c95e4ce17
sksamuel <sam@sksamuel.com>
2013-07-16 23:40:51 +0100

    Updated test for basket line update

--- src/test/scala/com/cloudray/scalapress/plugin/ecommerce/tag/BasketLineQtyTagTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/ecommerce/tag/BasketLineQtyTagTest.scala
@@ -11,21 +11,22 @@ import com.cloudray.scalapress.{ScalapressRequest, ScalapressContext}
 /** @author Stephen Samuel */
 class BasketLineQtyTagTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    val line1 = new BasketLine
-    line1.obj = new Obj
-    line1.qty = 2
-    line1.id = 15
-    line1.obj.price = 1000
-    line1.obj.vatRate = 15.00
+  val line1 = new BasketLine
+  line1.obj = new Obj
+  line1.qty = 2
+  line1.id = 15
+  line1.obj.price = 1000
+  line1.obj.vatRate = 15.00
 
-    val tag = new BasketLineQtyTag()
+  val tag = new BasketLineQtyTag()
 
-    val req = mock[HttpServletRequest]
-    val context = mock[ScalapressContext]
-    val sreq = new ScalapressRequest(req, context).withLine(line1)
+  val req = mock[HttpServletRequest]
+  val context = mock[ScalapressContext]
+  val sreq = new ScalapressRequest(req, context).withLine(line1)
 
-    test("tag renders qty input using id as input name") {
-        val actual = tag.render(sreq, Map.empty)
-        assert("<input type=\"text\" class=\"input-mini\" name=\"qty15\" value=\"2\"/>" === actual.get)
-    }
+  test("tag renders qty input using id as input name") {
+    val actual = tag.render(sreq, Map.empty)
+    assert( """<input type="number" min="1" max="100" step="1" class="input-mini" name="qty15" value="2"/>""" ===
+      actual.get)
+  }
 }
