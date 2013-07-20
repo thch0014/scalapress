4d59127f1660301f4e042bba72c04ab7b35fef6d
sksamuel <sam@sksamuel.com>
2013-07-20 13:40:17 +0100

    SP-155 updated nested folders tag to respect folder order

--- src/main/scala/com/cloudray/scalapress/folder/tag/NestedFoldersTag.scala
+++ src/main/scala/com/cloudray/scalapress/folder/tag/NestedFoldersTag.scala
@@ -2,7 +2,6 @@ package com.cloudray.scalapress.folder.tag
 
 import com.cloudray.scalapress.{Tag, ScalapressRequest}
 import collection.mutable.ArrayBuffer
-import scala.collection.JavaConverters._
 import com.cloudray.scalapress.folder.Folder
 import com.cloudray.scalapress.theme.tag.{ScalapressTag, TagBuilder}
 import com.cloudray.scalapress.util.UrlGenerator
@@ -11,32 +10,32 @@ import com.cloudray.scalapress.util.UrlGenerator
 @Tag("folders_nested")
 class NestedFoldersTag extends ScalapressTag with TagBuilder {
 
-    def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
+  def render(request: ScalapressRequest, params: Map[String, String]): Option[String] = {
 
-        val depth = params.get("depth").getOrElse("1").toInt
-        val excluded = params.get("exclude").getOrElse("").split(",")
-        val root = request.context.folderDao.root
-        val buffer = new ArrayBuffer[String]
-        buffer.append("<div class=\"nested-folders\">")
-        _renderLevel(root, depth - 1, buffer, excluded)
-        buffer.append("</div>")
-        val string = buffer.mkString("\n")
-        Some(string)
-    }
+    val depth = params.get("depth").getOrElse("1").toInt
+    val excluded = params.get("exclude").getOrElse("").split(",")
+    val root = request.context.folderDao.root
+    val buffer = new ArrayBuffer[String]
+    buffer.append("<div class=\"nested-folders\">")
+    _renderLevel(root, depth - 1, buffer, excluded)
+    buffer.append("</div>")
+    val string = buffer.mkString("\n")
+    Some(string)
+  }
 
-    private def _renderLevel(parent: Folder, depth: Int, buffer: ArrayBuffer[String], excluded: Array[String]) {
+  private def _renderLevel(parent: Folder, depth: Int, buffer: ArrayBuffer[String], excluded: Array[String]) {
 
-        val folders = parent.subfolders.asScala.toSeq
-        val filtered = folders.filterNot(f => excluded.contains(f.id.toString)).filterNot(_.hidden)
-        val sorted = filtered.sortBy(_.name)
-        buffer.append("<ul class=\"nested-folder-level" + depth + "\">")
-        sorted.foreach(child => {
-            buffer.append("<li id=\"nested-folder-" + child.id + "\">")
-            buffer.append(UrlGenerator.link(child))
-            if (depth > 0)
-                _renderLevel(child, depth - 1, buffer, excluded)
-            buffer.append("</li>")
-        })
-        buffer.append("</ul>")
-    }
+    val folders = parent.sortedSubfolders
+    val filtered = folders.filterNot(f => excluded.contains(f.id.toString)).filterNot(_.hidden)
+    val sorted = filtered.sortBy(_.name)
+    buffer.append("<ul class=\"nested-folder-level" + depth + "\">")
+    sorted.foreach(child => {
+      buffer.append("<li id=\"nested-folder-" + child.id + "\">")
+      buffer.append(UrlGenerator.link(child))
+      if (depth > 0)
+        _renderLevel(child, depth - 1, buffer, excluded)
+      buffer.append("</li>")
+    })
+    buffer.append("</ul>")
+  }
 }
