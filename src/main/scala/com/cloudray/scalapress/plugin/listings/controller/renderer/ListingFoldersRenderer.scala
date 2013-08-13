d723e72101661bdf6ca6a4674b92666ae036ea6d
sksamuel <sam@sksamuel.com>
2013-08-13 07:38:00 +0100

    Converted listing renderer to SSP

--- src/main/scala/com/cloudray/scalapress/plugin/listings/controller/renderer/ListingFoldersRenderer.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/listings/controller/renderer/ListingFoldersRenderer.scala
@@ -6,33 +6,32 @@ import com.cloudray.scalapress.plugin.listings.domain.{ListingsPlugin, ListingPr
 /** @author Stephen Samuel */
 object ListingFoldersRenderer {
 
-    def render(process: ListingProcess, plugin: ListingsPlugin, folders: Array[Folder]) = {
-        val max = process.listingPackage.maxFolders match {
-            case 0 => 1
-            case x: Int => x
-        }
-        val sorted = folders.sortBy(_.name)
-        val selects = (1 to max).map(arg => _select(sorted))
-        <div id="listing-process-folders">
-            {plugin.foldersPageText}
-            <form method="POST" action="/listing/folder">
-                {selects}<button type="submit" class="btn">Continue</button>
-            </form>
-        </div>
+  def render(process: ListingProcess, plugin: ListingsPlugin, folders: Array[Folder]) = {
+    val max = process.listingPackage.maxFolders match {
+      case 0 => 1
+      case x: Int => x
     }
+    val sorted = folders.sortBy(_.name)
+    val selects = (1 to max).map(arg => _select(sorted))
+    <div id="listing-process-folders">
+      {plugin.foldersPageText}<form method="POST" action="/listing/folder">
+      {selects}<button type="submit" class="btn">Continue</button>
+    </form>
+    </div>
+  }
 
-    private def _select(folders: Array[Folder]) =
-        <div class="listing-folder-selection">
-            <select name="folderId">
-                <option value="">None</option>{_options(folders)}
-            </select>
-        </div>
+  private def _select(folders: Array[Folder]) =
+    <div class="listing-folder-selection">
+      <select name="folderId">
+        <option value=" ">None</option>{_options(folders)}
+      </select>
+    </div>
 
-    private def _options(folders: Array[Folder]) = {
-        folders.map(f =>
-            <option value={f.id.toString}>
-                {f.name}
-            </option>)
-    }
+  private def _options(folders: Array[Folder]) = {
+    folders.map(f =>
+      <option value={f.id.toString}>
+        {f.name}
+      </option>)
+  }
 }
 
