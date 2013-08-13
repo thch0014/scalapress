81bd844219f08407b888f801f253a12de0919b3b
sksamuel <sam@sksamuel.com>
2013-08-13 23:05:06 +0100

    SP-159 fixed plugin account column lengths

--- src/main/scala/com/cloudray/scalapress/plugin/profile/AccountPlugin.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/profile/AccountPlugin.scala
@@ -20,13 +20,22 @@ class AccountPlugin {
   @ElementCollection
   @BeanProperty var accounts: java.util.List[java.lang.Long] = new java.util.ArrayList[java.lang.Long]()
 
+  @Column(length = 10000)
   @BeanProperty var accountPageHeader: String = _
+
+  @Column(length = 10000)
   @BeanProperty var accountPageFooter: String = _
 
+  @Column(length = 10000)
   @BeanProperty var loginPageHeader: String = _
+
+  @Column(length = 10000)
   @BeanProperty var loginPageFooter: String = _
 
+  @Column(length = 10000)
   @BeanProperty var registrationPageHeader: String = _
+
+  @Column(length = 10000)
   @BeanProperty var registrationPageFooter: String = _
 
   @BeanProperty var loginRedirect: String = _
