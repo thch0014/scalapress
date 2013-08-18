5a621bb96cb47cf97b483e7e370c413ef1dc30b2
sksamuel <sam@sksamuel.com>
2013-08-18 16:13:46 +0100

    Fixed calendar json output

--- src/main/scala/com/cloudray/scalapress/plugin/calendar/CalendarJsonController.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/calendar/CalendarJsonController.scala
@@ -1,27 +1,35 @@
 package com.cloudray.scalapress.plugin.calendar
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{PathVariable, ResponseBody, RequestMapping}
+import org.springframework.web.bind.annotation.{RequestMapping, PathVariable, ResponseBody}
 import org.springframework.beans.factory.annotation.Autowired
 import com.cloudray.scalapress.search.SearchService
 import com.cloudray.scalapress.ScalapressContext
-import scala.beans.BeanProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 /** @author Stephen Samuel */
 @Controller
 @RequestMapping(Array("plugin/calendar/event/"))
 class CalendarJsonController {
 
-    @Autowired var searchService: SearchService = _
-    @Autowired var context: ScalapressContext = _
+  @Autowired var searchService: SearchService = _
+  @Autowired var context: ScalapressContext = _
+  val mapper = new ObjectMapper
+  mapper.registerModule(DefaultScalaModule)
 
-    @RequestMapping(produces = Array("text/plain"), value = Array("widget/{id}"))
-    @ResponseBody
-    def json(@PathVariable("id") id: Long): Array[Event] = {
-        val widget = context.widgetDao.find(id).asInstanceOf[CalendarWidget]
-        val calendar = new CalendarService(searchService)
-        val events = calendar.events(widget.search, widget.startDateAttribute, Option(widget.endDateAttribute))
-        events.toArray
-    }
+  @RequestMapping(produces = Array("text/plain"), value = Array("widget/{id}"))
+  @ResponseBody
+  def json(@PathVariable("id") id: Long): String = {
+    val array = events(id)
+    mapper.writeValueAsString(array)
+  }
+
+  def events(@PathVariable("id") id: Long): Array[Event] = {
+    val widget = context.widgetDao.find(id).asInstanceOf[CalendarWidget]
+    val calendar = new CalendarService(searchService)
+    val events = calendar.events(widget.search, widget.startDateAttribute, Option(widget.endDateAttribute))
+    events.toArray
+  }
 }
 
--- src/test/scala/com/cloudray/scalapress/plugin/calendar/CalendarJsonControllerTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/calendar/CalendarJsonControllerTest.scala
@@ -11,41 +11,41 @@ import com.cloudray.scalapress.obj.attr.Attribute
 /** @author Stephen Samuel */
 class CalendarJsonControllerTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    val controller = new CalendarJsonController
-    val search = mock[SearchService]
-    val context = new ScalapressContext
-    context.widgetDao = mock[WidgetDao]
+  val controller = new CalendarJsonController
+  val search = mock[SearchService]
+  val context = new ScalapressContext
+  context.widgetDao = mock[WidgetDao]
 
-    controller.context = context
-    controller.searchService = search
+  controller.context = context
+  controller.searchService = search
 
-    val widget = new CalendarWidget
-    widget.startDateAttribute = new Attribute
-    widget.startDateAttribute.id = 200
+  val widget = new CalendarWidget
+  widget.startDateAttribute = new Attribute
+  widget.startDateAttribute.id = 200
 
-    val ref1 = new ObjectRef(1, 12, "coldplay", "live", Map(200l -> "1336025005000"), Nil)
-    val ref2 = new ObjectRef(2, 12, "jethro tull", "live", Map(200l -> "1336053300000"), Nil)
-    val ref3 = new ObjectRef(3, 13, "keane", "disabled", Map(200l -> "1335660004000"), Nil)
+  val ref1 = new ObjectRef(1, 12, "coldplay", "live", Map(200l -> "1336025005000"), Nil)
+  val ref2 = new ObjectRef(2, 12, "jethro tull", "live", Map(200l -> "1336053300000"), Nil)
+  val ref3 = new ObjectRef(3, 13, "keane", "disabled", Map(200l -> "1335660004000"), Nil)
 
-    Mockito.when(context.widgetDao.find(Matchers.anyLong)).thenReturn(widget)
+  Mockito.when(context.widgetDao.find(Matchers.anyLong)).thenReturn(widget)
 
-    val result = new SearchResult(Seq(ref1, ref2, ref3), Nil, 0)
-    Mockito.when(search.search(Matchers.any[SavedSearch])).thenReturn(result)
+  val result = new SearchResult(Seq(ref1, ref2, ref3), Nil, 0)
+  Mockito.when(search.search(Matchers.any[SavedSearch])).thenReturn(result)
 
-    test("calendar json happy path") {
-        val events = controller.json(4)
-        assert(3 === events.size)
+  test("calendar json happy path") {
+    val events = controller.events(4)
+    assert(3 === events.size)
 
-        assert("/object-1-coldplay" === events(0).url)
-        assert("/object-2-jethro-tull" === events(1).url)
-        assert("/object-3-keane" === events(2).url)
+    assert("/object-1-coldplay" === events(0).url)
+    assert("/object-2-jethro-tull" === events(1).url)
+    assert("/object-3-keane" === events(2).url)
 
-        assert("coldplay" === events(0).title)
-        assert("jethro tull" === events(1).title)
-        assert("keane" === events(2).title)
+    assert("coldplay" === events(0).title)
+    assert("jethro tull" === events(1).title)
+    assert("keane" === events(2).title)
 
-        assert("03/05/2012" === events(0).dateString)
-        assert("03/05/2012" === events(1).dateString)
-        assert("29/04/2012" === events(2).dateString)
-    }
+    assert("03/05/2012" === events(0).dateString)
+    assert("03/05/2012" === events(1).dateString)
+    assert("29/04/2012" === events(2).dateString)
+  }
 }
