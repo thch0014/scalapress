be3d5918c935ccce73f6959c3a2a201c238e0490
sksamuel <sam@sksamuel.com>
2013-08-26 23:50:49 +0100

    Added sales reporter tests

--- src/test/scala/com/cloudray/scalapress/plugin/ecommerce/SalesReporterTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/ecommerce/SalesReporterTest.scala
@@ -3,9 +3,10 @@ package com.cloudray.scalapress.plugin.ecommerce
 import org.scalatest.{FlatSpec, OneInstancePerTest}
 import org.scalatest.mock.MockitoSugar
 import com.cloudray.scalapress.obj.Obj
-import org.mockito.{ArgumentCaptor, Mockito}
+import org.mockito.{Matchers, ArgumentCaptor, Mockito}
 import com.cloudray.scalapress.plugin.ecommerce.controller.admin.OrderQuery
-import org.scalatest.junit.JUnitRunner
+import com.sksamuel.scoot.soa.Page
+import com.cloudray.scalapress.plugin.ecommerce.domain.Order
 
 /** @author Stephen Samuel */
 
@@ -14,11 +15,37 @@ class SalesReporterTest extends FlatSpec with MockitoSugar with OneInstancePerTe
   val reporter = new SalesReporter
   reporter.orderDao = mock[OrderDao]
 
+  Mockito.when(reporter.orderDao.search(Matchers.any[OrderQuery])).thenReturn(Page.empty[Order])
+
   "a sales report" should "query by date range" in {
     reporter.generate(Obj.STATUS_LIVE, 10000, 20000)
     val captor = ArgumentCaptor.forClass(classOf[OrderQuery])
     Mockito.verify(reporter.orderDao).search(captor.capture)
-    assert(captor.getValue.from === 10000)
-    assert(captor.getValue.to === 20000)
+    assert(captor.getValue.from.get === 10000)
+    assert(captor.getValue.to.get === 20000)
+  }
+
+  "a sales report" should "query by status" in {
+    reporter.generate("qweqwe", 10000, 20000)
+    val captor = ArgumentCaptor.forClass(classOf[OrderQuery])
+    Mockito.verify(reporter.orderDao).search(captor.capture)
+    assert(captor.getValue.status.get === "qweqwe")
+  }
+
+  "a sales report" should "be mapped to report line" in {
+    val order = new Order
+    order.id = 444
+    order.datePlaced = 1234124214
+    order.account = new Obj
+    order.account.name = "sammy"
+    order.account.email = "sammy@fatty.com"
+    Mockito.when(reporter.orderDao.search(Matchers.any[OrderQuery])).thenReturn(Page.apply(Seq(order)))
+
+    val reports = reporter.generate("qweqwe", 10000, 20000)
+    assert(1 === reports.size)
+    assert(1234124214 === reports(0).datePlaced)
+    assert("444" === reports(0).orderId)
+    assert("sammy@fatty.com" === reports(0).email)
+    assert("sammy" === reports(0).name)
   }
 }
