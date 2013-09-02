78e39117599a1babe07978db2df3ddef0770b077
sksamuel <sam@sksamuel.com>
2013-09-02 07:51:19 +0100

    added default password constant

--- src/main/scala/com/cloudray/scalapress/user/users.scala
+++ src/main/scala/com/cloudray/scalapress/user/users.scala
@@ -10,34 +10,36 @@ import com.cloudray.scalapress.util.{GenericDaoImpl, GenericDao}
 /** @author Stephen Samuel */
 
 trait UserDao extends GenericDao[User, java.lang.Long] {
-    def byUsername(s: String): User
+  def byUsername(s: String): User
 }
 
 @Component
 @Transactional
 class UserDaoImpl extends GenericDaoImpl[User, java.lang.Long] with UserDao {
-    @Transactional
-    def byUsername(uname: String): User = searchUnique(new Search(classOf[User]).addFilterEqual("username", uname))
+  @Transactional
+  def byUsername(uname: String): User = searchUnique(new Search(classOf[User]).addFilterEqual("username", uname))
 }
 
 @Component
 class UserDaoInit {
 
-    @Autowired var userDao: UserDao = _
+  val DEFAULT_HASH = "09b792e75d96dbcb3d49f5af313e9fa1"
 
-    @PostConstruct def ensureAUser() {
-        if (userDao.findAll().size == 0) {
-            val user = defaultUser
-            userDao.save(user)
-        }
-    }
+  @Autowired var userDao: UserDao = _
 
-    def defaultUser = {
-        val user = new User
-        user.username = "admin"
-        user.name = "admin"
-        user.passwordHash = "09b792e75d96dbcb3d49f5af313e9fa1"
-        user.active = true
-        user
+  @PostConstruct def ensureAUser() {
+    if (userDao.findAll().size == 0) {
+      val user = defaultUser
+      userDao.save(user)
     }
+  }
+
+  def defaultUser = {
+    val user = new User
+    user.username = "admin"
+    user.name = "admin"
+    user.passwordHash = DEFAULT_HASH
+    user.active = true
+    user
+  }
 }
