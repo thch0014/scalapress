2c696cb0ddcc31e17839abd6870ad20e27690b81
sksamuel <sam@sksamuel.com>
2013-09-22 12:45:36 +0100

    #22 Updated pooling sections to allow longer wait and more long lived idle connections

--- src/main/scala/com/cloudray/scalapress/settings/controller/GeneralSettingsController.scala
+++ src/main/scala/com/cloudray/scalapress/settings/controller/GeneralSettingsController.scala
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestMethod, ModelAttribute, RequestMapping}
 import org.springframework.beans.factory.annotation.Autowired
 import com.cloudray.scalapress.ScalapressContext
-import com.cloudray.scalapress.settings.{Installation, GeneralSettings, GeneralSettingsDao}
+import com.cloudray.scalapress.settings.{Installation, GeneralSettings}
 
 /** @author Stephen Samuel */
 @Controller
 @RequestMapping(Array("backoffice/settings/general"))
 class GeneralSettingsController {
 
-  @Autowired var generalSettingsDao: GeneralSettingsDao = _
   @Autowired var context: ScalapressContext = _
 
   @RequestMapping(method = Array(RequestMethod.GET), produces = Array("text/html"))
@@ -20,9 +19,9 @@ class GeneralSettingsController {
 
   @RequestMapping(method = Array(RequestMethod.POST), produces = Array("text/html"))
   def save(@ModelAttribute("settings") gsettings: GeneralSettings, @ModelAttribute site: Installation) = {
-    generalSettingsDao.save(gsettings)
+    context.generalSettingsDao.save(gsettings)
     edit(gsettings, site)
   }
 
-  @ModelAttribute("settings") def g = generalSettingsDao.findAll().head
+  @ModelAttribute("settings") def g = context.generalSettingsDao.findAll().head
 }
