fd05991feb8022f00236923b33a4ab68006275af
sksamuel <sam@sksamuel.com>
2013-09-27 07:36:39 +0100

    #35 Updated basket controller to save order first

--- src/main/scala/com/cloudray/scalapress/plugin/payments/paypal/standard/PaypalStandardProcessor.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/payments/paypal/standard/PaypalStandardProcessor.scala
@@ -10,104 +10,104 @@ import com.cloudray.scalapress.payments.{Transaction, Purchase, CallbackResult, 
 /** @author Stephen Samuel */
 class PaypalStandardProcessor(plugin: PaypalStandardPlugin) extends PaymentProcessor with Logging {
 
-    private val Sandbox = "https://www.sandbox.paypal.com/cgi-bin/webscr"
-    private val Production = "https://www.paypal.com/cgi-bin/webscr"
+  private val Sandbox = "https://www.sandbox.paypal.com/cgi-bin/webscr"
+  private val Production = "https://www.paypal.com/cgi-bin/webscr"
 
-    def paymentProcessorName = "PaypalStandard"
-    def paymentUrl: String = if (plugin.production) Production else Sandbox
+  def paymentProcessorName = "PaypalStandard"
+  def paymentUrl: String = if (plugin.production) Production else Sandbox
 
-    def params(domain: String, purchase: Purchase): Map[String, String] = {
+  def params(domain: String, purchase: Purchase): Map[String, String] = {
 
-        val params = scala.collection.mutable.Map[String, String]()
+    val params = scala.collection.mutable.Map[String, String]()
 
-        params += ("cmd" -> "_xclick")
-        params += ("currency_code" -> "GBP")
+    params += ("cmd" -> "_xclick")
+    params += ("currency_code" -> "GBP")
 
-        // Your PayPal ID or an email address associated with your PayPal account. Email addresses must be confirmed.
-        params += ("business" -> plugin.accountEmail)
+    // Your PayPal ID or an email address associated with your PayPal account. Email addresses must be confirmed.
+    params += ("business" -> plugin.accountEmail)
 
-        params += ("cancel_return" -> purchase.failureUrl)
-        params += ("return" -> purchase.successUrl)
+    params += ("cancel_return" -> purchase.failureUrl)
+    params += ("return" -> purchase.successUrl)
 
-        //The URL to which PayPal posts information about the payment, in the form of Instant Transaction Notification messages.
-        // todo setup paypal callback url
-        //        params += ("notify_url" -> (url + payable.callbackUrl + "?paymentPluginClass=" + classOf[PaypalStandardPlugin]
-        //        .getName))
+    //The URL to which PayPal posts information about the payment, in the form of Instant Transaction Notification messages.
+    // todo setup paypal callback url
+    //        params += ("notify_url" -> (url + payable.callbackUrl + "?paymentPluginClass=" + classOf[PaypalStandardPlugin]
+    //        .getName))
 
-        params += ("item_name" -> purchase.paymentDescription)
-        params += ("quantity" -> "1")
+    params += ("item_name" -> purchase.paymentDescription)
+    params += ("quantity" -> "1")
 
-        params += ("invoice" -> purchase.uniqueIdent)
-        params += ("custom" -> purchase.callbackInfo)
+    params += ("invoice" -> purchase.uniqueIdent)
+    params += ("custom" -> purchase.callbackInfo)
 
-        params += ("no_note" -> "1")
+    params += ("no_note" -> "1")
 
-        // The price or amount of the product, service, or contribution, not including shipping, handling, or tax.
-        // If this variable is omitted from Buy Now or Donate buttons, buyers enter their own amount at the time of payment.
-        val amount = "%.2f".format(purchase.total / 100.0)
-        params += ("amount" -> amount)
+    // The price or amount of the product, service, or contribution, not including shipping, handling, or tax.
+    // If this variable is omitted from Buy Now or Donate buttons, buyers enter their own amount at the time of payment.
+    val amount = "%.2f".format(purchase.total / 100.0)
+    params += ("amount" -> amount)
 
-        if (purchase.accountEmail != null)
-            params += ("email" -> purchase.accountEmail)
+    if (purchase.accountEmail != null)
+      params += ("email" -> purchase.accountEmail)
 
-        params.toMap
+    params.toMap
+  }
+
+  def _createTx(params: Map[String, String]): Transaction = {
+    logger.debug("Creating Tx from Params [{}]", params)
+
+    val transactionId = params("txn_id")
+    val amount = (params.get("mc_gross").filter(_.trim.length > 0).getOrElse("0").toDouble * 100).toInt
+
+    val tx = Transaction(transactionId, paymentProcessorName, amount)
+    tx.status = params("payment_status")
+    tx.currency = params("mc_currency")
+    tx.transactionType = params("payment_type")
+    tx.payerStatus = params("payer_status")
+    tx.paymentProcessor = paymentProcessorName
+    tx.payee = (params.get("first_name").getOrElse("") + " " + params.get("last_name").getOrElse("")).trim
+    tx.payeeEmail = params.get("payer_email").orNull
+    tx
+  }
+
+  def callback(params: Map[String, String]): Option[CallbackResult] = {
+    //    _isPaymentCompleted(params) match {
+    //        case false =>
+    //            logger.info("IPN callback not valid")
+    //            None
+    //       case true =>
+    //            logger.info("IPN callback is valid")
+    val tx = _createTx(params)
+    Some(CallbackResult(tx, _custom(params)))
+    //    }
+  }
+
+  def _custom(params: Map[String, String]): String = params("custom")
+
+  def _isGenuineCallback(params: Map[String, String]): Boolean = {
+
+    val queryParams = new BasicHttpParams()
+    queryParams.setParameter("cmd", "_notify-validate")
+    for ( (key, value) <- params ) {
+      queryParams.setParameter(key, value)
     }
 
-    def _createTx(params: Map[String, String]): Transaction = {
-        logger.debug("Creating Tx from Params [{}]", params)
+    val get = new HttpGet("https://www.paypal.com/cgi-bin/webscr")
+    get.setParams(queryParams)
 
-        val transactionId = params("txn_id")
-        val amount = (params.get("mc_gross").filter(_.trim.length > 0).getOrElse("0").toDouble * 100).toInt
+    val client = new DefaultHttpClient
+    val resp = client.execute(get)
 
-        val tx = Transaction(transactionId, paymentProcessorName, amount)
-        tx.status = params("payment_status")
-        tx.currency = params("mc_currency")
-        tx.transactionType = params("payment_type")
-        tx.payerStatus = params("payer_status")
-        tx.paymentProcessor = paymentProcessorName
-        tx.payee = (params.get("first_name").getOrElse("") + " " + params.get("last_name").getOrElse("")).trim
-        tx.payeeEmail = params.get("payer_email").orNull
-        tx
-    }
+    val string = EntityUtils.toString(resp.getEntity)
+    "VERIFIED".equalsIgnoreCase(string)
+  }
 
-    def callback(params: Map[String, String]): Option[CallbackResult] = {
-        //    _isPaymentCompleted(params) match {
-        //        case false =>
-        //            logger.info("IPN callback not valid")
-        //            None
-        //       case true =>
-        //            logger.info("IPN callback is valid")
-        val tx = _createTx(params)
-        Some(CallbackResult(tx, _custom(params)))
-        //    }
-    }
-
-    def _custom(params: Map[String, String]): String = params("custom")
-
-    def _isGenuineCallback(params: Map[String, String]): Boolean = {
-
-        val queryParams = new BasicHttpParams()
-        queryParams.setParameter("cmd", "_notify-validate")
-        for ( (key, value) <- params ) {
-            queryParams.setParameter(key, value)
-        }
-
-        val get = new HttpGet("https://www.paypal.com/cgi-bin/webscr")
-        get.setParams(queryParams)
-
-        val client = new DefaultHttpClient
-        val resp = client.execute(get)
-
-        val string = EntityUtils.toString(resp.getEntity)
-        "VERIFIED".equalsIgnoreCase(string)
-    }
-
-    def _isPaymentCompleted(params: Map[String, String]): Boolean = {
-        // check the callback was genuine from paypal
-        _isGenuineCallback(params) &&
-          // determines whether the transaction is complete
-          "COMPLETED".equalsIgnoreCase(params("payment_status")) &&
-          // Check email address to make sure that this is not a spoof
-          plugin.accountEmail.equals(params("receiver_email"))
-    }
+  def _isPaymentCompleted(params: Map[String, String]): Boolean = {
+    // check the callback was genuine from paypal
+    _isGenuineCallback(params) &&
+      // determines whether the transaction is complete
+      "COMPLETED".equalsIgnoreCase(params("payment_status")) &&
+      // Check email address to make sure that this is not a spoof
+      plugin.accountEmail.equals(params("receiver_email"))
+  }
 }
--- src/test/scala/com/cloudray/scalapress/plugin/ecommerce/controller/OrderStatusPopulatorTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/ecommerce/controller/OrderStatusPopulatorTest.scala
@@ -10,38 +10,38 @@ import org.mockito.Mockito
 /** @author Stephen Samuel */
 class OrderStatusPopulatorTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    val dao = mock[ShoppingPluginDao]
-    val plugin = new ShoppingPlugin
-    plugin.statuses = "dispatched,refunded"
-    Mockito.when(dao.get).thenReturn(plugin)
+  val dao = mock[ShoppingPluginDao]
+  val plugin = new ShoppingPlugin
+  plugin.statuses = "dispatched,refunded"
+  Mockito.when(dao.get).thenReturn(plugin)
 
-    val populator = new OrderStatusPopulator {
-        var shoppingPluginDao = dao
-    }
+  val populator = new OrderStatusPopulator {
+    var shoppingPluginDao = dao
+  }
 
-    test("status map includes default statuses") {
-        val statuses = populator._statuses
-        assert(statuses.contains(Order.STATUS_CANCELLED))
-        assert(statuses.contains(Order.STATUS_COMPLETED))
-        assert(statuses.contains(Order.STATUS_NEW))
-        assert(statuses.contains(Order.STATUS_PAID))
-    }
+  test("status map includes default statuses") {
+    val statuses = populator._statuses
+    assert(statuses.contains(Order.STATUS_CANCELLED))
+    assert(statuses.contains(Order.STATUS_COMPLETED))
+    assert(statuses.contains(Order.STATUS_NEW))
+    assert(statuses.contains(Order.STATUS_PAID))
+  }
 
-    test("status map includes custom statuses") {
-        val statuses = populator._statuses
-        assert(statuses.contains("dispatched"))
-        assert(statuses.contains("refunded"))
-    }
+  test("status map includes custom statuses") {
+    val statuses = populator._statuses
+    assert(statuses.contains("dispatched"))
+    assert(statuses.contains("refunded"))
+  }
 
-    test("status map sorts by key") {
-        val statuses = populator._statuses
-        val keys = statuses.keys.toSeq
-        // 0 index is empty for -status-
-        assert("Cancelled" === keys(1))
-        assert("Completed" === keys(2))
-        assert("dispatched" === keys(3))
-        assert("New" === keys(4))
-        assert("Paid" === keys(5))
-        assert("refunded" === keys(6))
-    }
+  test("status map sorts by key") {
+    val statuses = populator._statuses
+    val keys = statuses.keys.toSeq
+    // 0 index is empty for -status-
+    assert("Cancelled" === keys(1))
+    assert("Completed" === keys(2))
+    assert("dispatched" === keys(3))
+    assert("New" === keys(4))
+    assert("Paid" === keys(5))
+    assert("refunded" === keys(6))
+  }
 }
