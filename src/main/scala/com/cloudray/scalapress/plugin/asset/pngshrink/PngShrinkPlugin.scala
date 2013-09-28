dae5e8011436e7930491d018ef3887939507dd37
sksamuel <sam@sksamuel.com>
2013-09-28 21:46:06 +0100

    #38 added png optimizer plugin

---
+++ src/main/scala/com/cloudray/scalapress/plugin/asset/pngshrink/PngShrinkPlugin.scala
@@ -0,0 +1,37 @@
+package com.cloudray.scalapress.plugin.asset.pngshrink
+
+import java.io.{ByteArrayInputStream, InputStream}
+import org.springframework.stereotype.Component
+import com.cloudray.scalapress.media.AssetLifecycleListener
+import com.sksamuel.scrimage.{Format, Image}
+import com.cloudray.scalapress.Logging
+
+/** @author Stephen Samuel */
+@Component
+class PngShrinkPlugin extends AssetLifecycleListener with Logging {
+
+  def onStore(key: String, input: InputStream): (String, InputStream) = {
+
+    if (key.toLowerCase.endsWith(".png")) {
+
+      try {
+        val optimized = _optimize(input)
+        (key, optimized)
+      } catch {
+        case e: Exception =>
+          logger.warn("{}", e)
+          (key, input)
+      }
+
+    } else {
+      (key, input)
+    }
+
+  }
+
+  def _optimize(input: InputStream): InputStream = {
+    val image = Image(input)
+    val compressed = image.writer(Format.PNG).withMaxCompression.write()
+    new ByteArrayInputStream(compressed)
+  }
+}
