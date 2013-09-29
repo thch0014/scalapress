f277a6d2f7dfed6947b77170324dad47afd984c0
sksamuel <sam@sksamuel.com>
2013-09-29 07:13:45 +0100

    Added test for png shrink when input is invalid

--- src/test/scala/com/cloudray/scalapress/plugin/asset/pngshrink/PngShrinkPluginTest.scala
+++ src/test/scala/com/cloudray/scalapress/plugin/asset/pngshrink/PngShrinkPluginTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.{OneInstancePerTest, FunSuite}
 import org.scalatest.mock.MockitoSugar
 import org.apache.commons.io.IOUtils
 import com.sksamuel.scrimage.{Format, Image}
+import java.io.ByteArrayInputStream
 
 /** @author Stephen Samuel */
 class PngShrinkPluginTest extends FunSuite with MockitoSugar with OneInstancePerTest {
@@ -23,5 +24,11 @@ class PngShrinkPluginTest extends FunSuite with MockitoSugar with OneInstancePer
     val input = getClass.getResourceAsStream("/com/cloudray/scalapress/plugin/asset/pngshrink/tux.png")
     val (key, optimized) = plugin.onStore("tux.jpg", input)
     assert(image.write.size === unoptimized.size)
+  }
+
+  test("error while optimizing results in original stream") {
+    val input = new ByteArrayInputStream(Array[Byte](1, 2, 3, 4)) //invalid png
+    val (key, optimized) = plugin.onStore("tux.jpg", input)
+    assert(optimized === input)
   }
 }
