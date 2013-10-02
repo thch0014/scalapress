413f13bc1856790df68c05dc4a976ab72041a849
sksamuel <sam@sksamuel.com>
2013-10-02 23:15:34 +0100

    Fixed failing tag count test

--- src/test/scala/com/cloudray/scalapress/util/ComponentClassScannerTest.scala
+++ src/test/scala/com/cloudray/scalapress/util/ComponentClassScannerTest.scala
@@ -6,8 +6,8 @@ import org.scalatest.mock.MockitoSugar
 /** @author Stephen Samuel */
 class ComponentClassScannerTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    test("scanner picks up all tags") {
-        val tags = ComponentClassScanner.tags
-        assert(101 === tags.size)
-    }
+  test("scanner picks up all tags") {
+    val tags = ComponentClassScanner.tags
+    assert(102 === tags.size)
+  }
 }
