911d8e936e763ead35eba741c3f9494f079a8ef2
sksamuel <sam@sksamuel.com>
2013-10-18 21:43:07 +0100

    #84 fix for incorrect parameter in scalate template

--- src/main/scala/com/cloudray/scalapress/plugin/listings/domain/ListingsPlugin.scala
+++ src/main/scala/com/cloudray/scalapress/plugin/listings/domain/ListingsPlugin.scala
@@ -8,16 +8,14 @@ import scala.beans.BeanProperty
 @Table(name = "plugins_listings")
 class ListingsPlugin {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @BeanProperty var id: Long = _
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @BeanProperty var id: Long = _
 
-    //@Column(columnDefinition = "double default 0")
-    def vatRate: Double = 0
+  //@Column(columnDefinition = "double default 0")
+  def vatRate: Double = 0
 
-    @BeanProperty var packagesPageText: String = _
-    @BeanProperty var foldersPageText: String = _
-    @BeanProperty var imagesPageText: String = _
-
-    //def menu: Option[MenuItem] =    Some(MenuItem("Plugins", MenuItem("Listing Packages", "icon-th-large", "/backoffice/plugin/listings")))
+  @BeanProperty var packagesPageText: String = _
+  @BeanProperty var foldersPageText: String = _
+  @BeanProperty var imagesPageText: String = _
 }
