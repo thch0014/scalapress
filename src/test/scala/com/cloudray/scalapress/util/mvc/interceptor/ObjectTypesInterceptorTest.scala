24f360471b93b2fd74428e013c58335e7cc55eec
sksamuel <sam@sksamuel.com>
2013-10-20 01:25:09 +0100

    fixed tests

--- src/test/scala/com/cloudray/scalapress/util/mvc/interceptor/ObjectTypesInterceptorTest.scala
+++ src/test/scala/com/cloudray/scalapress/util/mvc/interceptor/ObjectTypesInterceptorTest.scala
@@ -12,24 +12,48 @@ import scala.collection.JavaConverters._
 /** @author Stephen Samuel */
 class ObjectTypesInterceptorTest extends FunSuite with MockitoSugar with OneInstancePerTest {
 
-    val dao = mock[TypeDao]
-    val interceptor = new ObjectTypesInterceptor(dao)
+  val dao = mock[TypeDao]
+  val interceptor = new ObjectTypesInterceptor(dao)
 
-    test("types do not include deleted") {
-        val type1 = new ObjectType
-        type1.deleted = false
-        val type2 = new ObjectType
-        type2.deleted = true
-        Mockito.when(dao.findAll()).thenReturn(List(type1, type2))
+  val req = mock[HttpServletRequest]
+  val resp = mock[HttpServletResponse]
+  val model = new ModelAndView
 
-        val req = mock[HttpServletRequest]
-        val resp = mock[HttpServletResponse]
-        val model = new ModelAndView
-        interceptor.postHandle(req, resp, null, model)
+  test("types do not include deleted") {
 
-        val types = model.getModelMap.get("types").asInstanceOf[util.Collection[ObjectType]].asScala.toSeq
-        assert(1 === types.size)
-        assert(type1 === types(0))
-    }
+    val type1 = new ObjectType
+    type1.name = "products"
+    type1.deleted = true
 
+    val type2 = new ObjectType
+    type2.name = "listings"
+    type2.deleted = false
+
+    Mockito.when(dao.findAll()).thenReturn(List(type1, type2))
+
+    interceptor.postHandle(req, resp, null, model)
+
+    val types = model.getModelMap.get("types").asInstanceOf[util.Collection[ObjectType]].asScala.toSeq
+    assert(1 === types.size)
+    assert(type2 === types(0))
+  }
+
+  test("types do not include accounts") {
+
+    val type1 = new ObjectType
+    type1.name = "products"
+    type1.deleted = false
+
+    val type2 = new ObjectType
+    type2.name = "account"
+    type2.deleted = false
+
+    Mockito.when(dao.findAll()).thenReturn(List(type1, type2))
+
+    interceptor.postHandle(req, resp, null, model)
+
+    val types = model.getModelMap.get("types").asInstanceOf[util.Collection[ObjectType]].asScala.toSeq
+    assert(1 === types.size)
+    assert(type1 === types(0))
+  }
 }
