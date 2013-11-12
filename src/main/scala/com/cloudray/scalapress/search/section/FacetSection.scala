7c694fa4c84bdd7d863b7f6a036ead66ca2f84c2
sksamuel <sam@sksamuel.com>
2013-11-12 17:27:20 +0000

    Fixed class cast error with facet

--- src/main/scala/com/cloudray/scalapress/search/section/FacetSection.scala
+++ src/main/scala/com/cloudray/scalapress/search/section/FacetSection.scala
@@ -7,7 +7,7 @@ import com.cloudray.scalapress.folder.Folder
 import com.cloudray.scalapress.search._
 import com.cloudray.scalapress.theme.MarkupRenderer
 import com.cloudray.scalapress.item.{Item, ItemDao}
-import scala.xml.Node
+import scala.xml.{Unparsed, Node}
 import com.github.theon.uri.Uri
 import com.cloudray.scalapress.search.AttributeFacetField
 import com.cloudray.scalapress.search.Facet
@@ -37,57 +37,15 @@ class FacetSection extends SearchResultsSection {
     val searchService = sreq.context.bean[SearchService]
     val result = searchService.search(search)
 
-    renderSelectedFacets(selections, uri) + renderFacets(result, uri) + renderItems(result, sreq)
-  }
-
-  private def renderSelectedFacets(selected: Seq[FacetSelection], uri: Uri): String = {
-    "<div class='search-selected-facets'>" +
-      selected.map(renderSelectedFacet(_, uri)).mkString("\n") +
-      "</div>"
-  }
-
-  private def field2name(field: FacetField): String = field match {
-    case AttributeFacetField(_) => "Attribute Name Here"
-    case TagsFacetField => "Tags"
-    case _ => ""
-  }
-
-  private def renderSelectedFacet(selected: FacetSelection, uri: Uri): String = {
-    val urlWithFacetRemoved = uri.removeParams(selected.field.key)
-    "<div class='search-selected-facet'>" + selected.value + " <a href=" + urlWithFacetRemoved + ">x</a>" + "</div>"
-  }
-
-  private def renderFacets(result: SearchResult, uri: Uri): String = {
+    val items = getItems(result, sreq.context.itemDao)
     val facetsWithMultipleTerms = result.facets.filter(_.terms.size > 1)
-    "<div class='search-facets'>" + facetsWithMultipleTerms.map(renderFacet(_, uri)).mkString("\n") + "</div>"
+
+    renderSelectedFacets(selections, uri) +
+      renderFacets(facetsWithMultipleTerms, uri) +
+      renderItems(items, sreq)
   }
 
-  private def renderFacet(facet: Facet, uri: Uri): Node = {
-    val terms = facet.terms.map(term =>
-      Scalate.layout(
-        "/com/cloudray/scalapress/search/section/facetterm.ssp",
-        Map("count" -> term.count,
-          "value" -> term.value,
-          "uri" -> uri.replaceParams(facet.field.key, term.value)
-        )
-      )
-    )
-    <div class="search-facet">
-      <h6>
-        {facet.name}
-      </h6>{terms}
-    </div>
-  }
-
-  private def renderItems(result: SearchResult, sreq: ScalapressRequest): String = {
-    val items = sreq.context.bean[ItemDao].findBulk(result.refs.map(_.id))
-    "<div class='search-items'>" + items.map(renderItem(_, sreq)).mkString("\n") + "</div>"
-  }
-
-  private def renderItem(item: Item, sreq: ScalapressRequest) = {
-    val m = Option(markup).getOrElse(item.itemType.objectListMarkup)
-    "<div class='search-item'>" + MarkupRenderer.render(item, m, sreq) + "</div>"
-  }
+  def getItems(result: SearchResult, dao: ItemDao): Seq[Item] = dao.findBulk(result.refs.map(_.id))
 
   private def createSearch(folder: Folder,
                            selections: Iterable[FacetSelection],
@@ -100,5 +58,43 @@ class FacetSection extends SearchResultsSection {
       case None => Nil
       case Some(ids) => ids.split(",").filterNot(_.isEmpty).map(id => AttributeFacetField(id.toLong))
     }
+  }
+
+  private def renderSelectedFacets(selected: Seq[FacetSelection], uri: Uri): String = {
+    "<div class='search-selected-facets'>" + selected.map(renderSelectedFacet(_, uri)).mkString("\n") + "</div>"
+  }
+
+  private def renderSelectedFacet(selected: FacetSelection, uri: Uri): String = {
+    val urlWithFacetRemoved = uri.removeParams(selected.field.key)
+    "<div class='search-selected-facet'>" + selected.value + " <a href=\"" + urlWithFacetRemoved + "\">x</a>" + "</div>"
+  }
+
+  private def renderFacets(facets: Iterable[Facet], uri: Uri): String = {
+    "<div class='search-facets'>" + facets.map(renderFacet(_, uri)).mkString("\n") + "</div>"
+  }
+
+  private def renderFacet(facet: Facet, uri: Uri): Node = {
+    val terms = facet.terms.map(term =>
+      Scalate.layout(
+        "/com/cloudray/scalapress/search/section/facetterm.ssp",
+        Map("count" -> term.count,
+          "value" -> term.value,
+          "url" -> uri.replaceParams(facet.field.key, term.value).toString())
+      )
+    )
+    <div class="search-facet">
+      <h6>
+        {facet.name}
+      </h6>{Unparsed(terms.mkString("\n"))}
+    </div>
+  }
+
+  private def renderItems(items: Seq[Item], sreq: ScalapressRequest): String = {
+    "<div class='search-items'>" + items.map(renderItem(_, sreq)).mkString("\n") + "</div>"
+  }
+
+  private def renderItem(item: Item, sreq: ScalapressRequest) = {
+    val m = Option(markup).getOrElse(item.itemType.objectListMarkup)
+    "<div class='search-item'>" + MarkupRenderer.render(item, m, sreq) + "</div>"
   }
 }
